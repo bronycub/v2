4ec532c9821f963fd1725eef59a79b58b9312c6e
Lezarius <contact@starry.pw>
2014-03-08 13:26:28 +0100

    Adding template source (menu and footer) for integrade this use iclude php

---
+++ menu.php
@@ -0,0 +1,40 @@
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="BootStrap/js/bootstrap.min.js"></script>
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+
+    
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="#NONE" class="navbar-brand">#NONE</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li>
+              <a href="#NONE">#NONE</a>
+            </li>
+            
+             <li>
+              <a href="#NONE">#NONE</a>
+            </li>
+            <li><a href="#NONE">#NONE</a></li>
+            <li>
+              <a href="#NONE">#NONE</a>
+            </li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#NONE">#NONE</a></li>
+          </ul>
+
+        </div>
+      </div>
+   </nav>
