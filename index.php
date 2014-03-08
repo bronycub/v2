b70aa99a41b5b6a7508ff0c0356717564290d1c4
Lezarius <contact@starry.pw>
2014-03-08 17:01:21 +0100

    Index modif

--- index.php
+++ index.php
@@ -17,7 +17,23 @@
 </p>
   </div> 
 <hr>
-	<a href="#TEST"><h1 align="center">WIP</h1></a>
+	<div class="row">
+	<div class="container">
+	<div class="col-md-6">
+	<h2>BronyCUB, Qu'est-ce que c'est ?</h2>
+	<div class="jumbotron">
+		<article>BronyCUB est un collectif de Bronies de la Communauté Urbaine de Bordeaux en Gironde.
+Ce collectif a pour ambition de réunir les fans pour organiser divers Meet-up</article>
+</br>
+	<button type="button" class="btn btn-info btn-lg">Nous contactez</button>
+	</div>
+		<div class="col-md-6">
+			</div>
+
+		</div>
+
+</div>
+	</div>
 
 
 
