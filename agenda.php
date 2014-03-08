1333e362edf5f6fa5c56ce9099fb507218af7b50
Lezarius <contact@starry.pw>
2014-03-08 18:11:45 +0100

    Adding page for menu

--- agenda.php
+++ agenda.php
@@ -4,20 +4,20 @@
 	<meta charset="UTF-8">
 	<title>Agenda</title>
 	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="../BootStrap/css/bootstrap.css"> 
- 	<link rel="stylesheet" type="text/css" href="../BootStrap/css/footer.css">
+	<link rel="stylesheet" type="text/css" href="BootStrap/css/bootstrap.css"> 
+ 	<link rel="stylesheet" type="text/css" href="BootStrap/css/footer.css">
 
-	<link rel="stylesheet" type="text/css" media="screen" href="calendar/css/smoothness/jquery-ui-1.8.23.custom.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="calendar/css/calendar.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="agenda/calendar/css/smoothness/jquery-ui-1.8.23.custom.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="agenda/calendar/css/calendar.css" />
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script type="text/javascript" src="calendar/js/jquery-ui-1.8.23.custom.min.js"></script>
-	<script type="text/javascript" src="calendar/js/calendar.min.js"></script>
+	<script type="text/javascript" src="agenda/calendar/js/jquery-ui-1.8.23.custom.min.js"></script>
+	<script type="text/javascript" src="agenda/calendar/js/calendar.min.js"></script>
 
 </head>
 <body>
 
-	<?php include '../menu.php'; ?>
+	<?php include 'menu.php'; ?>
 
 	<div class="container-fluid">
 		<div class="row">
@@ -27,8 +27,8 @@
 		 <div class="col-md-8">
 			<?php
 		
-			require_once('calendar/class/calendar.class.php');
-			require_once('calendar/class/ical.class.php');
+			require_once('agenda/calendar/class/calendar.class.php');
+			require_once('agenda/calendar/class/ical.class.php');
 
 			$cal = new Calendar(array(
 			'url' 			=> "https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.ics" ,
@@ -46,5 +46,5 @@
 		</div>
 	</div>
 
-	<?php include '../footer.php'; ?>
+	<?php include 'footer.php'; ?>
 </body>
---
+++ partenaire.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WIP</title>
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="BootStrap/css/bootstrap.css"> 
+  <link rel="stylesheet" type="text/css" href="BootStrap/css/footer.css">
+</head>
+<body>
+
+	<?php include 'menu.php'; ?>
+	<div class="row">
+<h1>WIP</h1>
+	</div>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
