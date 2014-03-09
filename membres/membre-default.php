13d083c2d4da5cc6e7fb54993d7f2d8c9704146d
Lezarius <contact@starry.pw>
2014-03-09 20:52:09 +0100

    testing some change

---
+++ membres/membre-default.php
@@ -0,0 +1,7 @@
+          <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet">
+              <link rel="stylesheet" type="text/css" href="../BootStrap/css/bootstrap.css"> 
+              <link rel="stylesheet" type="text/css" href="../BootStrap/css/footer.css">
+			  
+<?php include '../header.php'; ?>
+
+<?php include '../footer.php'; ?>
