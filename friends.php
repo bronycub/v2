38710ca2a5f6c6838576ee11c5bcad48c48a8c8d
heuzef <contact@heuzef.com>
2014-03-11 11:04:41 +0100

    Ajouts de contenus sur les pages join ; membres et friends

--- friends.php
+++ friends.php
@@ -1,35 +1,126 @@
 <?php include 'header.php'; ?>
 	<div class="container">
-		
-		<!-- Projet crée/codé -->
 		<div class="row">
-			<h3>Partenaire brony cub :</h3>	
 
-
-		</div>
-
-
-		<div class="row">
 			<div class="col-md-4">
-				<h1>MLP fiction:</h1>
-				<a href="http://mlpfiction.com/" target="New"><img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/mlpficpartlogo.png" alt="" ></a>
-				<article>MLP Fictions est le 1er site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony!</article>
-
-			</div>
-  			<div class="col-md-4">
-  				<h1>Le Poney Blanc</h1>
-				<a href="http://cv.remy-mazabraud.eu/" target="New"><img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/poneyblancpartlogo.png" alt=""></a>
-				<article>Site d'échange et d'information sur My Little Pony : Friendship is Magic pour les bronies francophones.</article>
-  			</div>
-  			<div class="col-md-4">
-				<h1>En attente</h1>
-				<a href="#none"> <img src="http://placehold.it/300x200" alt=""></a>
-				<article>Lol nope.
+				<h1>MLP Fiction</h1>
+				<a href="http://mlpfiction.com" target="_blank">
+				<img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/mlpficpartlogo.png">
+				</a>
+				<article>
+				MLP Fictions est le 1er site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony!
 				</article>
-  			</div>	
-		</div>
+			</div>
 
+			<div class="col-md-4">
+				<h1>Le Poney Blanc</h1>
+				<a href="http://leponeyblanc.fr" target="_blank">
+				<img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/poneyblancpartlogo.png">
+				</a>
+				<article>
+				MLP Fictions est le 1er site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony!
+				</article>
+			</div>
 
+			<div class="col-md-4">
+				<h1>New Lunar Républic</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Radio Brony</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Pulps</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Mandora</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Edil</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Brony.fr</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Ternoc</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Troll Me Tender</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Dragons trésors et comptes</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
+
+			<div class="col-md-4">
+				<h1>Canterlot Corner</h1>
+				<a href="#" target="_blank">
+				<img src="http://fakeimg.pl/300x200/">
+				</a>
+				<article>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
+				</article>
+			</div>
 		</div>
 	</div>
-	<?php include 'footer.php'; ?>
+<?php include 'footer.php'; ?>
