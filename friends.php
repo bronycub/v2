6bd6a25e62d6eb6c9bca6882366d1c255ffbc0d8
heuzef <contact@heuzef.com>
2014-03-12 17:51:24 +0100

    Amélioration de la page friends.php

--- friends.php
+++ friends.php
@@ -2,125 +2,151 @@
 	<div class="container">
 		<div class="row">
 
-			<div class="col-md-4">
-				<h1>MLP Fiction</h1>
-				<a href="http://mlpfiction.com" target="_blank">
-				<img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/mlpficpartlogo.png">
-				</a>
-				<article>
-				MLP Fictions est le 1er site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony!
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-info">
+				  <div class="panel-heading">Le Poney Blanc</div>
+				  <div class="panel-body">
+				    <a href="http://leponeyblanc.fr" target="_blank">
+					<img class="thumbnail" src="./img/le_poney_blanc.png">
+				    </a>
+				    <p>Site d'échange et d'information sur My Little Pony : Friendship is Magic pour les bronies francophones.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Le Poney Blanc</h1>
-				<a href="http://leponeyblanc.fr" target="_blank">
-				<img src="http://www.radiobrony.fr/wp-content/uploads/2012/08/poneyblancpartlogo.png">
-				</a>
-				<article>
-				MLP Fictions est le 1er site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony!
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">New Lunar Républic</div>
+				  <div class="panel-body">
+				    <a href="http://newlunarrepublic.fr" target="_blank">
+					<img class="thumbnail" src="./img/new_lunar_republic.png">
+				    </a>
+				    <p>Site ayant pour but de faciliter l'accès aux différents contenus de la série MLP:FIM tel que les vidéos en streaming, les musiques et même les livres.</p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>New Lunar Républic</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">MLP Fiction</div>
+				  <div class="panel-body">
+				    <a href="http://mlpfictions.com" target="_blank">
+				    <img class="thumbnail" src="./img/mlp_fiction.png">
+				    </a>
+				    <p>Le 1<sup>er</sup> site francophone dédié à la lecture de fictions autours de l'univers de My Little Pony !<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Radio Brony</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-success">
+				  <div class="panel-heading">Radio Brony</div>
+				  <div class="panel-body">
+				    <a href="http://radiobrony.fr" target="_blank">
+				    <img class="thumbnail" src="./img/radio_brony.png">
+				    </a>
+				    <p>La radio des Bronies, 100% Française !<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Pulps</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Pulps</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Mandora</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Mandora</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Edil</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Edil</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Brony.fr</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Troll Me Tender</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Ternoc</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Dragons trésors et comptes</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Troll Me Tender</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Canterlot Corner</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Dragons trésors et comptes</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Brony.fr</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
 
-			<div class="col-md-4">
-				<h1>Canterlot Corner</h1>
-				<a href="#" target="_blank">
-				<img src="http://fakeimg.pl/300x200/">
-				</a>
-				<article>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit. Quisque sit amet imperdiet dui. In lacinia nunc lorem, a fringilla ipsum pretium quis. Donec lobortis sem magna, id laoreet dui aliquam nec.
-				</article>
+			<div class="col-md-4" align="center">
+				<div class="panel panel-default">
+				  <div class="panel-heading">Ternoc</div>
+				  <div class="panel-body">
+				    <a href="#" target="_blank">
+					<img class="thumbnail" src="http://fakeimg.pl/300x200/">
+				    </a>
+				    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vestibulum pulvinar orci laoreet hendrerit.<br /><br /></p>
+				  </div>
+				</div>
 			</div>
+
+
 		</div>
 	</div>
 <?php include 'footer.php'; ?>
