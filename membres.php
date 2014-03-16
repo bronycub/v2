3441b760fd2a28aed820521de85a6b94f2bc203e
Lezarius <contact@starry.pw>
2014-03-17 00:40:13 +0100

    starting template for membre

--- membres.php
+++ membres.php
@@ -1,7 +1,72 @@
 <?php include 'header.php'; ?>
 <div class="contenair">
-	<div class="row">
-		<h1 align="center">Qu'elle que chosse.</h1>
+<div class="row">
+	<div class="col-lg-1"></div>
+	<div class="col-lg-5">
+		
+
+<a href="#" class="list-group-item">
+   <table class="table">
+ 
+    <tr>
+      <th width="20%">
+      <p align="left"><img src="http://fr.gravatar.com/avatar/73e4bdc45fa3c3c053a3e837d2fef772?s=125" alt="" class="thumbnail img-responsive"></p>
+      </th>
+      <th width="30%">
+      <h2 align="left">Lezarius</h2>
+		<h5 align="left">Rémy Mazabraud</h5>
+			<h5>30/01/1996</h5>
+	  </th>
+      <th width="50%">Column heading</th>
+    </tr>
+ 
+  
+  
+</table> 
+  </a>
+
+
 	</div>
+	<div class="col-lg-5">
+		
+<a href="#" class="list-group-item">
+   <table class="table">
+ 
+    <tr>
+      <th width="20%">
+      <p align="left"><img src="http://fr.gravatar.com/avatar/73e4bdc45fa3c3c053a3e837d2fef772?s=125" alt="" class="thumbnail img-responsive"></p>
+      </th>
+      <th width="30%">
+      <h2 align="left">Lezarius</h2>
+		<h5 align="left">Rémy Mazabraud</h5>
+			<h5>30/01/1996</h5>
+	  </th>
+      <th width="50%">
+										<a href="skype:alexouil4?call" target="_blank" class="zocial icon skype"></a>
+										<a href="http://www.youtube.com/channel/UCqX2vIbM82qFLaqyg0studw/videos?view=1&feature=guide" target="_blank" class="zocial icon youtube"></a>
+										<a href="https://plus.google.com/100034201101510718430" target="_blank" class="zocial icon googleplus"></a>
+										<br>
+										<a href="https://twitter.com/RagatorStilwell" target="_blank" class="zocial icon twitter"></a> 
+										<a href="https://www.facebook.com/Ragator" target="_blank" class="zocial icon facebook"></a>
+										<a href="http://alexouil4.deviantart.com" target="_blank" class="zocial icon disqus"></a>
+										<br>
+										<a href="http://ragator.tumblr.com" target="_blank" class="zocial icon tumblr"></a>
+										<a href="http://steamcommunity.com/profiles/76561198025372514?l=french" target="_blank" class="zocial icon steam"></a>
+										<a href="http://www.betaseries.com/membre/Ragator" target="_blank" class="zocial icon googleplay"></a>
+      </th>
+    </tr>
+    <tr>
+    <th></th>	
+    </tr>
+ 
+  
+  
+</table> 
+  </a>
+
+	</div>
+	<div class="col-lg-1"></div>
 </div>
+</div>
+
 <?php include 'footer.php'; ?>
