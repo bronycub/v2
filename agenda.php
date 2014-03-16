6ecd02f157cbb98600210cb60bcf8669e3bd3781
heuzef <contact@heuzef.com>
2014-03-17 00:42:48 +0100

    MAJ du header, de l'agenda, du readme et création de la page event

--- agenda.php
+++ agenda.php
@@ -1,38 +1,28 @@
-<head>
-
-	<link rel="stylesheet" type="text/css" media="screen" href="calendar/css/smoothness/jquery-ui-1.8.23.custom.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="calendar/css/calendar.css" />
-
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-	<script type="text/javascript" src="calendar/js/jquery-ui-1.8.23.custom.min.js"></script>
-	<script type="text/javascript" src="calendar/js/calendar.min.js"></script>
-
-</head>
-
 <?php include 'header.php'; ?>
+	<div align="center" class="container-fluid">
+		<a href="event.php"><button type="button" class="btn btn-primary btn-lg">Organiser un &eacute;venement</button></a>
+		<hr />
+	</div>
 	<div class="container-fluid">
 		<div class="row">
-		 <div class="col-md-3">
-		</div>
+			<div class="col-md-3"></div>
 
-		 <div class="col-md-6">
-			<?php
-			require_once('calendar/class/calendar.class.php');
-			require_once('calendar/class/ical.class.php');
+			 <div class="col-md-6">
+				<?php
+				require_once('calendar/class/calendar.class.php');
+				require_once('calendar/class/ical.class.php');
 
-			$cal = new Calendar(array(
-			'url' 			=> "https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.ics" ,
-			'scale'       		 => 'year' ,
-			'date_day_addition' 	=> false ,
-			));
+				$cal = new Calendar(array(
+				'url' 			=> "https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.ics" ,
+				'scale'       		 => 'year' ,
+				'date_day_addition' 	=> false ,
+				));
 
-			 $cal->output(); 
-			?>
-		</div>
+				 $cal->output(); 
+				?>
+			</div>
 
-		<div class="col-md-3">
-		</div>
+			<div class="col-md-3"></div>
 		</div>
 	</div>
-
 <?php include 'footer.php'; ?>
