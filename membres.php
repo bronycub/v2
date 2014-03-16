52216f947d2c2fff46e0752b4dd91f7bb7127045
Lezarius <contact@starry.pw>
2014-03-16 22:06:12 +0100

    Vtf flo <3

--- membres.php
+++ membres.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <div class="contenair">
 	<div class="row">
-		<h1 align="center">Les membres du collectif</h1>
+		<h1 align="center">Qu'elle que chosse.</h1>
 	</div>
 </div>
 <?php include 'footer.php'; ?>
