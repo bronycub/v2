91d8e40c940a976ae9fa3cef6adf6feab64f3377
Lezarius <contact@starry.pw>
2014-03-17 22:42:57 +0100

    Changing TH to TD

--- membres.php
+++ membres.php
@@ -8,15 +8,15 @@
    <table class="table">
  
     <tr>
-      <th width="20%">
+      <td width="20%">
       <p align="left"><img src="http://fr.gravatar.com/avatar/c0f08ae603e364cb554fe71c9fc94ffd?s=125" alt="" class="thumbnail img-responsive"></p>
-      </th>
-      <th width="30%">
+      </td>
+      <td width="30%">
       <h2 align="left">Heuzef</h2>
 		<h5 align="left">Florent Heuzé</h5>
 			<h5>05/07/1990</h5>
-	  </th>
-      <th width="50%">
+	  </td>
+      <td width="50%">
       	<div class="None" align="right">
 										<a href="skype:alexouil4?call" target="_blank" class="zocial icon skype"></a>
 										<a href="http://www.youtube.com/channel/UCqX2vIbM82qFLaqyg0studw/videos?view=1&feature=guide" target="_blank" class="zocial icon youtube"></a>
@@ -30,18 +30,18 @@
 										<a href="http://steamcommunity.com/profiles/76561198025372514?l=french" target="_blank" class="zocial icon steam"></a>
 										<a href="http://www.betaseries.com/membre/Ragator" target="_blank" class="zocial icon googleplay"></a>
       </div>
-      </th>
+      </td>
     </tr>
     </table>
     <table class="table">
     <tr>
-    <th width="100%">
+    <td width="100%">
     <div class="expandable">
     <p>	Fondateur du collectif BronyCUB, Florent est ce que l'on appelle un Geek. Avec le soutien de l'association <a href="http://www.mandora.fr" target="_blank">Mandora</a>, il a créé ce collectif suite aux difficultés rencontrées par les Bronies habitants la CUB à se regrouper et organiser des rencontres.
 									Florent vit à Bordeaux depuis plusieurs années, où il à commencer <a href="http://www.heuzef.com" target="_blank">sa carrière dans l'informatique</a>, il découvrit également une forte passion pour l'<a href="http://www.campus-aikido.fr" target="_blank">Aïkido</a>.
 									</p>
     </div>
-    </th>	
+    </td>	
     </tr>
  
   
@@ -56,15 +56,15 @@
    <table class="table">
  
     <tr>
-      <th width="20%">
+      <td width="20%">
       <p align="left"><img src="http://fr.gravatar.com/avatar/73e4bdc45fa3c3c053a3e837d2fef772?s=125" alt="" class="thumbnail img-responsive"></p>
-      </th>
-      <th width="30%">
+      </td>
+      <td width="30%">
       <h2 align="left">Lezarius</h2>
 		<h5 align="left">Rémy Mazabraud</h5>
 			<h5>30/01/1996</h5>
-	  </th>
-      <th width="50%">
+	  </td>
+      <td width="50%">
       	<div class="None" align="right">
 										<a href="skype:alexouil4?call" target="_blank" class="zocial icon skype"></a>
 										<a href="http://www.youtube.com/channel/UCqX2vIbM82qFLaqyg0studw/videos?view=1&feature=guide" target="_blank" class="zocial icon youtube"></a>
@@ -78,16 +78,16 @@
 										<a href="http://steamcommunity.com/profiles/76561198025372514?l=french" target="_blank" class="zocial icon steam"></a>
 										<a href="http://www.betaseries.com/membre/Ragator" target="_blank" class="zocial icon googleplay"></a>
       </div>
-      </th>
+      </td>
     </tr>
     </table>
     <table class="table">
     <tr>
-    <th width="100%">
+    <td width="100%">
     <div class="expandable">
     	<p>Artiste dans divers domaines ! Adorateur du Steampunk ! Développeur amateur et blogueur ! Yaoiste et Otaku assumé :3 Cette bio est trop courte, comme ma planète ! Sinon il <a href="https://twitter.com/remy_mazabraud" target="_blank">tweet</a> des trucs.</p>
     </div>
-    </th>	
+    </td>	
     </tr>
  
   
