90cc99e3764041d5195b6c81557fa2658c6be399
Lezarius <contact@starry.pw>
2014-03-17 22:36:57 +0100

    fix bug

---
+++ script/expender.js
@@ -0,0 +1,17 @@
+
+// you can override default options globally, so they apply to every .expander() call
+$.expander.defaults.slicePoint = 50;
+
+$(document).ready(function() {
+  
+
+  // override default options (also overrides global overrides)
+  $('div.expandable p').expander({
+    slicePoint:       50,  // default is 100
+    expandPrefix:     ' ', // default is '... '
+    expandText:       '[...]', // default is 'read more'
+    collapseTimer:    0, // re-collapses after 5 seconds; default is 0, so no re-collapsing
+    userCollapseText: '[^]'  // default is 'read less'
+  });
+
+});
