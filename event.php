d7693bf11c8e516163b60b2cd4794b7c5af3f586
heuzef <contact@heuzef.com>
2014-03-18 00:15:33 +0100

    Avancement page event.php

--- event.php
+++ event.php
@@ -1,10 +1,10 @@
 <?php
 if(isset($_POST['submit'])) {
 
-	if(trim($_POST['nomevent']) == '') {
+	if(trim($_POST['nom_event']) == '') {
 		$hasError = true;
 	} else {
-		$name = trim($_POST['nomevent']);
+		$name = trim($_POST['nom_event']);
 	}
 
 	if(trim($_POST['prenom']) == '') {
@@ -97,8 +97,8 @@ if(isset($_POST['submit'])) {
 		<div class="row">
 		 <div class="col-md-2">
 		</div>
-
 		 <div class="col-md-8">
+		 <img src="./img/pinkie_pie_party.gif" style="float:right; margin-right:10px;" />
 			<form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform" class="form-horizontal well" enctype="multipart/form-data">
 				<?php if(isset($hasError)) { ?>
 				<div class="alert alert-dismissable alert-danger"><button type="button" class="close" data-dismiss="alert">×</button>Certains champs ne sont pas remplis avec des informations valides.</div>
@@ -107,7 +107,7 @@ if(isset($_POST['submit'])) {
 
 				<?php if(isset($emailSent) && $emailSent == true) { ?>
 				<div class="alert alert-dismissable alert-success"><button type="button" class="close" data-dismiss="alert">×</button>
-					<h3>Message bien reçu !</h3>
+					<h3>Party !!</h3>
 					<iframe width="853" height="480" src="//www.youtube.com/embed/UPAuS3bfOzU?autoplay=1" frameborder="0" allowfullscreen></iframe>
 				</div>
 				<br />
@@ -115,19 +115,23 @@ if(isset($_POST['submit'])) {
 				<?php } ?>
 
 				<div class="form-group">
-					<label for="nomevent" class="col-lg-2 control-label">Nom de l'évenement</label>
-					<div class="col-lg-3"><input type="text" name="nomevent" id="nomevent" class="form-control" id="nomevent" placeholder="Nom de l'&eacute;venement" aria-required="true" ></div>
+					<label for="nom_event" class="col-lg-1 control-label">Nom</label>
+					<div class="col-lg-2"><input type="text" name="nom_event" id="nom_event" class="form-control" id="nom_event" placeholder="Nom de l'&eacute;venement" aria-required="true" ></div>
+					<label for="date_event" class="col-lg-1 control-label">Date</label>
+					<div class="col-lg-2"><input type="text" name="date_event" id="date_event" class="form-control" id="date_event" placeholder="Date de l'&eacute;venement" aria-required="true" ></div>
+					<label for="date_event" class="col-lg-1 control-label">Lieu</label>
+					<div class="col-lg-2"><input type="text" name="lieu_event" id="lieu_event" class="form-control" id="lieu_event" placeholder="Lieu de l'&eacute;venement" aria-required="true" ></div>
 				</div>
 				<br />
 				<div class="form-group">
-					<label for="bio" class="col-lg-1 control-label">Pr&eacute;sentation</label>
-					<div class="col-lg-9"><textarea maxlength="500" rows="5" name="bio" id="bio" rows="6" class="form-control" role="textbox" aria-required="true" placeholder="Ecrivez quelques lignes &agrave; propos de vous (500 caract&eacute;res max)"></textarea></div>
+					<label for="bio" class="col-lg-1 control-label">D&eacute;scription</label>
+					<div class="col-lg-9"><textarea maxlength="500" rows="15" name="bio" id="bio" rows="6" class="form-control" role="textbox" aria-required="true">Lorem ipsum dolor sit amet</textarea></div>
 				</div>
 				<br />
 				<div class="form-group">
 					<div class="col-lg-4"></div>
 					<div class="col-lg-4">
-					<input type="submit" value="BROHOOF /)" name="submit" id="submitButton" class="btn btn-info btn-large" title="Envoyer !" />
+					<input type="submit" value="PARTY !!" name="submit" id="submitButton" class="btn btn-info btn-large" title="Envoyer !" />
 					</div>
 					<div class="col-lg-4"></div>
 				</div>
