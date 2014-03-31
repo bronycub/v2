221dc76dc479b49ccea2753f249305646f0d7bf4
heuzef <contact@heuzef.com>
2014-03-31 17:16:04 +0200

    avancement du profil membre

--- membres.php
+++ membres.php
@@ -1,50 +1,42 @@
+<?php
+if ($handle = opendir('.')) {
+    while (false !== ($entry = readdir($handle))) {
+	if ($entry != "." && $entry != ".." && $entry != "index.php") {
+			if ($sub_handle = opendir($entry)) { 
+				while (false !== ($sub_entry = readdir($sub_handle))) { 
+				if ($sub_entry != "." && $sub_entry != "..") { 
+				echo '<p><a href="'.$entry.'/'.$sub_entry.'">'.$sub_entry.'</a></p>';
+				}
+			    }
+			    closedir($sub_handle);
+			}
+	}
+    }
+    closedir($handle);
+}
+?>
+
+
 <?php include 'header.php'; ?>
-<!-- Start global contenair -->
 
-
-<!-- Row start -->
 <div class="container">
-	
-	<!-- Profils 1 -->
+
 	<div class="col-lg-6">
-
-<?php include 'membres/heuzef.php'; ?>
-
+		<?php include 'membres/heuzef.php'; ?>
 	</div>
-	<!-- Profils 2 -->
+
 	<div class="col-lg-6">
-
-	<?php include 'membres/lezarius.php'; ?>
-
-</div>
+		<?php include 'membres/heuzef.php'; ?>
+	</div>
 
 </div>
-<!-- Row fin -->
-</div>
 
-
-<!-- Row start -->
 <div class="container">
-	
-	<!-- Profils 1 -->
+
 	<div class="col-lg-6">
-
-<?php include 'membres/heuzef.php'; ?>
-
+		<?php include 'membres/heuzef.php'; ?>
 	</div>
-	<!-- Profils 2 -->
-	<div class="col-lg-6">
-
-	<?php include 'membres/lezarius.php'; ?>
 
 </div>
-
-</div>
-<!-- Row fin -->
-</div>
-
-
-
-
 
 <?php include 'footer.php'; ?>
