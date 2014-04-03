2d1fd65c3be1404e1875153a6136b66106910280
heuzef <contact@heuzef.com>
2014-04-03 12:13:27 +0200

    commit add --all

---
+++ membres/ragator_stilwell.php
@@ -0,0 +1,22 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Ragator&nbsp;Stilwell";
+$gravatar="fae51fe4bff477e55d8f4ca4d92c8133";
+$prenom="Alexandre";
+$nom="Carralou";
+$naissance="01/09/1995";
+$url="
+<a href='skype:alexouil4?call' target='_blank' class='zocial icon skype'></a>
+<a href='http://www.youtube.com/channel/UCqX2vIbM82qFLaqyg0studw/videos?view=1&feature=guide' target='_blank' class='zocial icon youtube'></a>
+<a href='https://plus.google.com/100034201101510718430' target='_blank' class='zocial icon googleplus'></a>
+<a href='https://twitter.com/RagatorStilwell' target='_blank' class='zocial icon twitter'></a>
+<a href='https://www.facebook.com/Ragator' target='_blank' class='zocial icon facebook'></a>
+<a href='http://alexouil4.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='http://ragator.tumblr.com' target='_blank' class='zocial icon tumblr'></a>
+<a href='http://steamcommunity.com/profiles/76561198025372514?l=french' target='_blank' class='zocial icon steam'></a>
+<a href='http://www.betaseries.com/membre/Ragator' target='_blank' class='zocial icon googleplay'></a>
+";
+$bio="Alexandre est un geek doublé d'un nerd, passionné par les jeux vidéos, il souhaite plus que tout au monde que la réalité virtuelle se développe, il fait actuellement des études en informatique en espérant travailler dans la programmation logiciel. Ermite, émotionnel et perfectionniste, Alexandre aime beaucoup la <a href='http://www.youtube.com/channel/UCqX2vIbM82qFLaqyg0studw/videos?view=1&feature=guide' target='_blank'>musique</a> et il souhaite faire découvrir son monde passionnant.";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is YAY !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
---
+++ membres/seb_whooves.php
@@ -0,0 +1,16 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Seb&nbsp;Whooves";
+$gravatar="60e9503c8ca1bf7c8e9a27850bc74b85";
+$prenom="Sébastien";
+$nom="Brettnacher";
+$naissance="06/07/1982";
+$url="
+<a href='https://plus.google.com/104822943670669050631' target='_blank' class='zocial icon googleplus'></a>
+<a href='https://www.facebook.com/sebastien.brettnacher' target='_blank' class='zocial icon facebook'></a>
+<a href='http://chupacabra371.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+";
+$bio="Fan de manga et de comics, Sebastien est Brony depuis 2012 et souhaite rencontrer et partager ses passions. Collectionneur compulsif de figurines en tous genre et <a href='http://chupacabra371.deviantart.com' target='_blank'>dessinateur amateur</a>, Seb est un type riche en surprise !";
+$pony="<img src='img/ponies/lyra_heartstring.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Lyra is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
