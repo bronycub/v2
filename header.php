4161f9e470801195136e694010f4d52fa7276c68
heuzef <contact@heuzef.com>
2014-04-03 15:00:07 +0200

    correction des 404 dans le header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 <body>
 	<!-- All JS script -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="script/js/bootstrap.min.js"></script>
+	<script src="script/bootstrap.min.js"></script>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script type="text/javascript" src="script/calendar/js/jquery-ui-1.8.23.custom.min.js"></script>
 	<script type="text/javascript" src="script/calendar/js/calendar.min.js"></script>
@@ -21,7 +21,7 @@
    
 	<link rel="stylesheet" type="text/css" media="screen" href="script/calendar/css/smoothness/jquery-ui-1.8.23.custom.css" />
 	<link rel="stylesheet" type="text/css" media="screen" href="script/calendar/css/calendar.css" />
-	<link href="script/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="css_global/css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 
 	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 		<div class="container-fluid">
