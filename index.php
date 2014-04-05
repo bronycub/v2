8e5c9af3dd7fc5a7c68906cead99e10d11e2bdc9
heuzef <contact@heuzef.com>
2014-04-05 21:25:05 +0200

    structure page accueil

--- index.php
+++ index.php
@@ -17,75 +17,48 @@
 	<hr />
 	<div class="container">
 		<div class="col-md-6">
+			<h2>BronyCUB, Qu'est-ce que c'est ?</h2>
 			<div class="jumbotron">
+				<article>
+					BronyCUB est un collectif de Bronies de la <strong>C</strong>ommunauté <strong>U</strong>rbaine de <strong>B</strong>ordeaux en Gironde.
+					<br /><br />
+					Ce collectif a pour ambition de réunir les fans pour organiser diverses rencontres.
+					<br /><br />
+				</article>
+				
 				<!-- Question -->
-				<a class="linkhome" data-toggle="modal" data-target="#modal">BronyCUB, Qu'est-ce que c'est ?</a>
-					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					  <div class="modal-dialog">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					        <h4 class="modal-title" id="myModalLabel">BronyCUB, Qu'est-ce que c'est ?</h4>
-					      </div>
-					      <div class="modal-body">
-					      	BronyCUB est un collectif de Bronies de la <strong>C</strong>ommunauté <strong>U</strong>rbaine de <strong>B</strong>ordeaux en Gironde. <br />
-					      	Ce collectif a pour ambition de réunir les fans pour organiser diverses rencontres.
-					      </div>
-					    </div>
-					  </div>
+				<a data-toggle="modal" data-target="#modal"><button type="button" class="btn btn-primary btn-lg">Plus d'info ...</button></a>
+					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="plusinfos" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+									<h4 class="modal-title" id="plusinfos">Plus d'info ...</h4>
+								</div>
+								<div class="modal-body">
+								<h4>BronyCUB, Qu'est-ce que c'est ?</h4>
+								BronyCUB est un collectif de Bronies de la <strong>C</strong>ommunauté <strong>U</strong>rbaine de <strong>B</strong>ordeaux en Gironde.
+								<br />
+								Ce collectif a pour ambition de réunir les fans pour organiser diverses rencontres.
+								<hr />
+								<h4>Question</h4>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer venenatis sapien eu ullamcorper mattis. Suspendisse ac egestas nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique tempor eleifend. Sed quis lorem quis velit facilisis ornare. Etiam tempor porttitor purus, non aliquet magna mattis sed. Donec tincidunt turpis tempus, suscipit arcu a, porttitor sapien.
+								<hr />
+								<h4>Question</h4>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer venenatis sapien eu ullamcorper mattis. Suspendisse ac egestas nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique tempor eleifend. Sed quis lorem quis velit facilisis ornare. Etiam tempor porttitor purus, non aliquet magna mattis sed. Donec tincidunt turpis tempus, suscipit arcu a, porttitor sapien.
+								<hr />
+								<h4>Question</h4>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer venenatis sapien eu ullamcorper mattis. Suspendisse ac egestas nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique tempor eleifend. Sed quis lorem quis velit facilisis ornare. Etiam tempor porttitor purus, non aliquet magna mattis sed. Donec tincidunt turpis tempus, suscipit arcu a, porttitor sapien.
+								<hr />
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				<!-- /Question -->
-				<hr />
-				<!-- Question -->
-				<a class="linkhome" data-toggle="modal" data-target="#modal">Lorem ipsum dolor sit.</a>
-					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					  <div class="modal-dialog">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					        <h4 class="modal-title" id="myModalLabel">Lorem ipsum dolor sit.</h4>
-					      </div>
-					      <div class="modal-body">
-					      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quibusdam?
-					      </div>
-					    </div>
-					  </div>
-					</div>
-				<!-- /Question -->
-				<hr />
-				<!-- Question -->
-				<a class="linkhome" data-toggle="modal" data-target="#modal">Lorem ipsum dolor sit.</a>
-					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					  <div class="modal-dialog">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					        <h4 class="modal-title" id="myModalLabel">Lorem ipsum dolor sit.</h4>
-					      </div>
-					      <div class="modal-body">
-					      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quibusdam?
-					      </div>
-					    </div>
-					  </div>
-					</div>
-				<!-- /Question -->
-				<hr />
-				<!-- Question -->
-				<a class="linkhome" data-toggle="modal" data-target="#modal">Lorem ipsum dolor sit.</a>
-					<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					  <div class="modal-dialog">
-					    <div class="modal-content">
-					      <div class="modal-header">
-					        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-					        <h4 class="modal-title" id="myModalLabel">Lorem ipsum dolor sit.</h4>
-					      </div>
-					      <div class="modal-body">
-					      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, quibusdam?
-					      </div>
-					    </div>
-					  </div>
-					</div>
-				<!-- /Question -->
+ 
 			</div>
 		</div>
 		<div class="col-md-6">
