9aaef1b8d55748a11735e49f71316aa1d02e877a
ragator <ragator.stilwell@gmail.com>
2014-04-27 19:13:45 +0200

    ajout du membre calimeroor

---
+++ membres/calimeroor.php
@@ -0,0 +1,18 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Calimeroor";
+$gravatar="f298f961d1ca0d39418c06f50e6e85bb";
+$prenom="Stanislas";
+$nom="Rousselot";
+$naissance="08/07/1997";
+$url="
+<a href='https://www.youtube.com/user/Calimeroor' target='_blank' class='zocial icon youtube'></a>
+<a href='https://plus.google.com/+StanScrash/posts' target='_blank' class='zocial icon googleplus'></a>
+<a href='https://twitter.com/Calimeroor' target='_blank' class='zocial icon twitter'></a>
+";
+$bio="Je suis un jeune graphiste, j'habite a Toulouse mais je peut venir � Bordeaux quand je veux ...
+J'aime tous les styles de musique, de manga, de dessin anim�, de film, je suis tr�s ouvert ^^
+Voilivoilou !";
+$pony="<img src='img/ponies/48.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Soarin is best pony !</span>;
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
