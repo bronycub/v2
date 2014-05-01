57d200a4e94cb3d9357c5a73c2db01ba40daa6ea
heuzef <contact@heuzef.com>
2014-05-01 20:42:19 +0200

    maj du profil de ravien

--- membres/ravien.php
+++ membres/ravien.php
@@ -1,7 +1,7 @@
 ﻿<?php //INFOS DU MEMBRES
 //////////////////////////////////////////////
 $pseudo="Ravien";
-$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$gravatar="928c6541b37814d99730ca85b4d801a3";
 $prenom="Jérémy";
 $nom="Barret";
 $naissance="26/09/1998";
@@ -9,8 +9,7 @@ $url="
 <a href='http://steamcommunity.com/id/ravien33' target='_blank' class='zocial icon steam'></a>
 ";
 $bio="Jeremy est un gamer depuis qu'il a 2 ans ! Brony depuis debut de la saison 3 !
-Il adore lire Stephen King en particulier et regarde diverses séries (principalement Doctor Who et South Park).
-Il joue de la guitare électrique avec son groupe et réalise avec eux des reprises lors de concert organisé par une école. Jeremy viens tout juste d'entré au lycée.";
-$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
+Il adore lire Stephen King en particulier et regarde diverses séries (principalement Doctor Who et South Park). Jeremy viens tout juste d'entré au lycée.";
+$pony="<img src='img/ponies/18.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Sweetie Belle is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
