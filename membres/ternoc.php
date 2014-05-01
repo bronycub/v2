6c54d19ea926fcbe28ca053f11eb47066ceb086d
heuzef <contact@heuzef.com>
2014-05-01 21:04:49 +0200

    ajout du membre ternoc

---
+++ membres/ternoc.php
@@ -0,0 +1,20 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Ternoc";
+$gravatar="0cdeae60807ab588e6d343bf94604215";
+$prenom="Horace";
+$nom="Laval";
+$naissance="01/01/1980";
+$url="
+<a href='http://ternoc.com' target='_blank' class='zocial icon chrome'></a>
+<a href='http://tern.fr/g' target='_blank' class='zocial icon googleplus'></a>
+<a href='http://tern.fr/t' target='_blank' class='zocial icon twitter'></a>
+<a href='http://tern.fr/y' target='_blank' class='zocial icon youtube'></a>
+<a href='http://tern.fr/f' target='_blank' class='zocial icon flickr'></a>
+<a href='http://tern.fr/dev' target='_blank' class='zocial icon bitbucket'></a>
+<a href='https://steamcommunity.com/groups/trncom' target='_blank' class='zocial icon steam'></a>
+";
+$bio="34 ans et surveillant dans un lycée. Je suis un Gamer, brony, codeur fou et graphiste amateur. Aussi créateur de conneries. N'hésitez pas à jetter un oeil à mon <a href='http://ternoc.com' target='_blank'>site</a> ! Who is best poney ? Rainb*paf* Derpy !";
+$pony="<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
