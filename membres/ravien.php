a17041804f46f7a7992256b6760d42f6777e5f2b
heuzef <contact@heuzef.com>
2014-05-01 21:07:24 +0200

    correction fautes

--- membres/ravien.php
+++ membres/ravien.php
@@ -9,7 +9,7 @@ $url="
 <a href='http://steamcommunity.com/id/ravien33' target='_blank' class='zocial icon steam'></a>
 ";
 $bio="Jeremy est un gamer depuis qu'il a 2 ans ! Brony depuis debut de la saison 3 !
-Il adore lire Stephen King en particulier et regarde diverses séries (principalement Doctor Who et South Park). Jeremy viens tout juste d'entré au lycée.";
+Il adore lire Stephen King en particulier et regarde diverses séries (principalement Doctor Who et South Park). Jeremy vient tout juste d'entrée au lycée.";
 $pony="<img src='img/ponies/18.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Sweetie Belle is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
