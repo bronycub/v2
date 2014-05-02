5f0e306d7d45aa278a6fb87d96d7a37454652a9a
heuzef <contact@heuzef.com>
2014-05-03 00:12:23 +0200

    Ajout du membre Yolteotl

---
+++ membres/yolteotl.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Yolteotl";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Vincent";
+$nom="Offner";
+$naissance="22/08/1990";
+$url="
+";
+$bio="Je suis alsacien actuellement en stage de fin d'études d'ingénieur à Thalès, je compte bien resté dans le coin vu que la ville me plait beaucoup. Je suis Brony depuis la saison 2 et présent sur Bordeaux depuis février 2014.";
+$pony="<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best pony !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
