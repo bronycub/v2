6fe127eefb3b08034f4bbc791cfdaae71db6bed4
heuzef <contact@heuzef.com>
2014-05-05 19:33:06 +0200

    ajout du membre MaCharon

---
+++ membres/macharon.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="MaCharon";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Kévin";
+$nom="Pessina";
+$naissance="12/12/1992";
+$url="
+";
+$bio="Kévin est Brony depuis peu et découvre encore cette communauté grâce à certaines personnes rencontrées à l'Animasia. Otaku et geek, il aime et cherche à faire de nouvelles rencontres pour égayer ses journées.";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
