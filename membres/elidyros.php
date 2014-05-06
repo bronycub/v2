fd6cbd409eb1c697f402eb5732c020183c8db15c
heuzef <contact@heuzef.com>
2014-05-06 16:09:05 +0200

    Ajout du membre Elidyros

---
+++ membres/elidyros.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Elidyros";
+$gravatar="fdfa81403e870f68e58543514db43df7";
+$prenom="Nicolas";
+$nom="S.";
+$naissance="04/03/1987";
+$url="
+<a href='https://twitter.com/Elidyros' target='_blank' class='zocial icon twitter'></a>
+";
+$bio="Nicolas à découvert la série très récemment et est rapidement devenu fan. Il a la fâcheuse tendance à se jeter sur les produits dérivés ... Il est aussi passionné de jeux vidéos, en particulier de fight et musicaux. Il dessine, fais un peu de 3D, de musique, bref, il a tendance à toucher à tout.";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
