93887ed04828161198fd55bba74b73d64b227d28
Heuzef <contact@heuzef.com>
2014-05-08 14:09:11 +0200

    ajout du membre klembrony

---
+++ membres/klembrony.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Klembrony";
+$gravatar="cf20950f7bebe7b92934088e413965d7";
+$prenom="Clément";
+$nom="Klemoyu";
+$naissance="17/12/1998";
+$url="
+<a href='https://twitter.com/klemoyu' target='_blank' class='zocial icon twitter'></a>
+<a href='https://www.facebook.com/klemoyu.sert' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="J'ai découvert les poneys grâce à la NLR et franchement je suis heureux d'avoir fais cette découverte extraordinaire. Ils ont changé ma vie. Je suis un No-Life, j'adore Radio Brony et Pokemon ^^";
+$pony="<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
