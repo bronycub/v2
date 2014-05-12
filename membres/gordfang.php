f2104108cb9ab5b098148359e779f50ce48919f4
heuzef <contact@heuzef.com>
2014-05-12 14:09:57 +0200

    ajout du membre Gordfang

---
+++ membres/gordfang.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Gordfang";
+$gravatar="18f6346a75cea58a79c76314d2a9dbd2";
+$prenom="Baptiste";
+$nom="Cazaubon";
+$naissance="16/12/1994";
+$url="
+";
+$bio="Baptiste est un brony mais surtout un gamer (plus pour dragon age et dark souls) et otaku (plus pour evangelion et log horizon). Si vous avez besoin d'aide, vous pouvez compter sur lui !";
+$pony="<img src='img/ponies/octavia.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Octavia is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
