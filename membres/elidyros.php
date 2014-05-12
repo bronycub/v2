53ea2323a5828a2f4afa2394fd2e52f2230554d0
heuzef <contact@heuzef.com>
2014-05-12 23:17:07 +0200

    maj fiche elidyros

--- membres/elidyros.php
+++ membres/elidyros.php
@@ -7,8 +7,11 @@ $nom="S.";
 $naissance="04/03/1987";
 $url="
 <a href='https://twitter.com/Elidyros' target='_blank' class='zocial icon twitter'></a>
+<a href='https://twitter.com/Elidyros' target='_blank' class='zocial icon facebook'></a>
+<a href='skype:live:elidyros?call' target='_blank' class='zocial icon skype'></a>
+<a href='http://raptr.com/ELiDYROS' target='_blank' class='zocial icon chrome'></a>
 ";
 $bio="Nicolas à découvert la série très récemment et est rapidement devenu fan. Il a la fâcheuse tendance à se jeter sur les produits dérivés ... Il est aussi passionné de jeux vidéos, en particulier de fight et musicaux. Il dessine, fais un peu de 3D, de musique, bref, il a tendance à toucher à tout.";
-$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/38.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Chrysalis is best changeling !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
