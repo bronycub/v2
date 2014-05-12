847eb3f78df5e09f061aeafb0bbd5cdc993cd02b
heuzef <contact@heuzef.com>
2014-05-12 17:14:40 +0200

    maj fiche Gordfang

--- membres/gordfang.php
+++ membres/gordfang.php
@@ -6,6 +6,11 @@ $prenom="Baptiste";
 $nom="Cazaubon";
 $naissance="16/12/1994";
 $url="
+<a href='https://www.facebook.com/baptiste.cazaubon' target='_blank' class='zocial icon facebook'></a>
+<a href='http://steamcommunity.com/id/Madfox40' target='_blank' class='zocial icon steam'></a>
+<a href='skype:Madfox94?call' target='_blank' class='zocial icon skype'></a>
+<a href='http://baptisteorlenard.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='https://www.youtube.com/user/Orlenard' target='_blank' class='zocial icon youtube'></a>
 ";
 $bio="Baptiste est un brony mais surtout un gamer (plus pour dragon age et dark souls) et otaku (plus pour evangelion et log horizon). Si vous avez besoin d'aide, vous pouvez compter sur lui !";
 $pony="<img src='img/ponies/octavia.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Octavia is best pony !</span>";
