6c9536bd44546d63fb97a3e96c74a3bb1f5c6485
heuzef <contact@heuzef.com>
2014-05-14 08:49:21 +0200

    ajout du membre ElvinaZarah

---
+++ membres/elvinazarah.php
@@ -0,0 +1,16 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="ElvinaZarah";
+$gravatar="a23fd127da46f17c40660bbe47f77953";
+$prenom="Elvina";
+$nom="Mokrani";
+$naissance="07/05/1989";
+$url="
+<a href='http://elvinazarah.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='https://www.facebook.com/elvina.zarah.dreamcatcher' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Bin je suis un pitit poney trop mignon, fan de Pokemon et de Yaoi (aucun lien entre les deux...) =D
+Et pis j'suis auxiliaire de vie sur le Bassin d'Arcachon. Pis c'est tout. Adoptez moi =D";
+$pony="<span class='label label-info'>ElvinaZarah is best Pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
