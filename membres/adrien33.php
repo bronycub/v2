73380b6f386a17e0f3ec7d8d6f1e6f930f2a7723
heuzef <contact@heuzef.com>
2014-05-16 14:27:57 +0200

    ajout de adriens33

---
+++ membres/adrien33.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Adriens33";
+$gravatar="66a614defdfde3f9e15191cf4d76c05c";
+$prenom="Adrien";
+$nom="Sohier";
+$naissance="22/02/1992";
+$url="
+<a href='https://www.art-software.fr' target='_blank' class='zocial icon chrome'></a>
+<a href='skype:adrien.s33?call' target='_blank' class='zocial icon skype'></a>
+<a href='https://github.com/adrien-s' target='_blank' class='zocial icon github'></a>
+<a href='http://git.art-software.fr' target='_blank' class='zocial icon github'></a>
+";
+$bio="Un drôle de mix entre un geek râleur et qui aime bien faire les choses par lui même et un brony :D Question caractère, c'est une sorte de mélange entre Fluttershy et Twilight Sparkle.";
+$pony="<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
