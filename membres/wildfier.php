394cd10676971bb83ed8c40e1730237789ee0c77
heuzef <contact@heuzef.com>
2014-05-18 22:08:17 +0200

    edit wildfire

--- membres/wildfier.php
+++ membres/wildfier.php
@@ -1,6 +1,6 @@
 <?php //INFOS DU MEMBRES
 //////////////////////////////////////////////
-$pseudo="wildfier";
+$pseudo="Wildfier";
 $gravatar="3cc1bc48b0ded8921c14a5fc73c8a578";
 $prenom="Matthias";
 $nom="";
@@ -11,7 +11,7 @@ $url="
 <a href='https://github.com/mdevlamynck' target='_blank' class='zocial icon github'></a>
 <a href='http://steamcommunity.com/id/wildfier' target='_blank' class='zocial icon steam'></a>
 ";
-$bio="Métaleux, développeur et gamer. Si vous appréciez Dream Theater et Symphony X, le C++ et Linux, Dark Souls et Valve, on vas s'entendre sans problèmes ! Et puis si c'est pas le cas aussi ;).";
+$bio="Métaleux, développeur et gamer. Si vous appréciez Dream Theater et Symphony X, le C++ et Linux, Dark Souls et Valve, on va s'entendre sans problèmes ! Et puis si ce n'est pas le cas ... on va s'entendre quand même ;)";
 $pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony, yay !</span>&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Princess Luna is best Princess !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
