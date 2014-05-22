81635b1196a4072ff89fc12e7953d7a434cb3a23
heuzef <contact@heuzef.com>
2014-05-22 22:18:35 +0200

    ajout du membre eclipse

---
+++ membres/eclipse2212.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Eclipse2212";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Lucas";
+$nom="Assier";
+$naissance="22/12/1997";
+$url="
+<a href='https://plus.google.com/u/0/117735217572182705469' target='_blank' class='zocial icon googleplus'></a>
+<a href='https://twitter.com/LucasASSIER' target='_blank' class='zocial icon twitter'></a>
+<a href='https://www.facebook.com/lucas.assier.1' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Brony depuis environ un an, Lucas passe sa vie sur le PC ... enfin quand ses parents sont absents ! Il aime aussi lire des fanfics.
+Lucas est un roux, il n'a donc pas d'âme, pour ceux qui n'aiment pas les roux, il va falloir le supporter !";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
---
+++ membres/valcab33.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="ValCab33";
+$gravatar="72ce3b5bf63e88f7e988a4598e2efceb";
+$prenom="Valentin";
+$nom="Caballero";
+$naissance="02/10/1996";
+$url="
+";
+$bio="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, totam aspernatur doloremque impedit illo velit quo laboriosam ullam error assumenda quod dolorem voluptate officia repellat voluptates sunt quam perspiciatis a.";
+$pony="<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Princess Twilight Sparkle is best alicorn !</span>&nbsp;&nbsp;<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
