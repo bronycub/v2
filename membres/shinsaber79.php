795ae873c9bc3c29080a552dfeca92505ae25c0a
heuzef <contact@heuzef.com>
2014-05-27 12:11:15 +0200

    ajout du membre shinsaber

---
+++ membres/shinsaber79.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Shinsaber79";
+$gravatar="b7517f5e6ebff8e9d9229fcf544d6d98";
+$prenom="Adrien";
+$nom="Audemar";
+$naissance="03/04/1992";
+$url="
+<a href='http://steamcommunity.com/profiles/76561198014595710' target='_blank' class='zocial icon steam'></a>
+<a href='skype:shinsaber79?call' target='_blank' class='zocial icon skype'></a>
+";
+$bio="Arrivée dans la communauté Brony en voulant devenir amis avec un métaleux ... Souvent, des personnes se demandent combien je suis dans ma tête, car je suis très varié dans mes caractères, parfois calme, souvent gamin, rarement méchant et bien d'autre.
+
+J'aime bien jouer, principalement a Portal 1/2 ou des jeux avec des amis. Je suis aussi un fan de séries japonaises, mais plutôt les petites séries méconnues qui n'ont pas 300 épisodes ^^ Pour finir, car il faut bien une fin, je dirais juste : Unix vaincras !";
+$pony="<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy hooves is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
