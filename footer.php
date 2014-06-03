4984200265c7f8d382b1e6ca1137ec98c003794b
heuzef <contact@heuzef.com>
2014-06-03 15:13:23 +0200

    ajout du module humanitaires dans le footer

--- footer.php
+++ footer.php
@@ -2,28 +2,21 @@
 <br />
 <footer>
 	<div class="container-fluid">
-		<div class="col-md-2"></div>
+		<div class="col-md-1"></div>
 		<div class="col-md-3">
 			<img src="./img/derpy.gif" width="50px" style="float:right;" />
 			<h4><i class="fa fa-envelope"></i>&nbsp;&nbsp;Contactez-nous :</h4>
 			<a class="btn btn-primary btn-lg" href="mailto:bronycub@googlegroups.com">bronycub@googlegroups.com</a>
 		</div>
-		<div class="col-md-3"></div>
-		<div class="col-md-3">
-			<br />
-			<ul class="list-unstyled list-inline list-social-icons">
-				<li class="tooltip-social google-plus-link">
-				<a href="https://plus.google.com/u/0/+Bronycub" data-toggle="tooltip" data-placement="top" title="Google+" target="_blank"><i class="fa fa-google-plus-square fa-5x"></i></a>
-				</li>
-				<li class="tooltip-social twitter-link">
-				<a href="https://twitter.com/bronycub" data-toggle="tooltip" data-placement="top" title="Twitter" target="_blank"><i class="fa fa-twitter-square fa-5x"></i></a>
-				</li>
-				<li class="tooltip-social facebook-link">
-				<a href="https://www.facebook.com/Bronycub" data-toggle="tooltip" data-placement="top" title="Facebook" target="_blank"><i class="fa fa-facebook-square fa-5x"></i></a>
-				</li>
-			</ul>
+		<div class="col-md-2">
 		</div>
-		<div class="col-md-2"></div>
+		<div class="col-md-5">
+			<h4><i class="fa fa-heart"></i>&nbsp;&nbsp;Prochaine action humanitaire :</h4>
+			<div class="well">
+				<?php include('footer.txt') ?>
+			</div>
+		</div>
+		<div class="col-md-1"></div>
 	</div>
 </footer>
 
