9ac9a2b23f9e36a0ec9bbf92d0cb8b0620a6ce43
heuzef <contact@heuzef.com>
2014-06-04 17:16:06 +0200

    ajout membre Ludvann

---
+++ membres/ludvann.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Ludvann";
+$gravatar="4b46b54ce38364aebcd7fe66644486c7";
+$prenom="Antonin";
+$nom="Dupont";
+$naissance="17/10/1998";
+$url="
+";
+$bio="Lycéen, pratique de l'escade, guitare et corne muse (oui la poche avec les tuyaux). Antonin aime bien, pardon, il adore l'informatique et l'electronique (et surtout les <em>Equus caballus</em>, mais chut, c'est un secret ^^).";
+$pony="<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
