bdcf53acf0a5fc8334351f67fd144f77bac9f059
heuzef <contact@heuzef.com>
2014-06-14 16:06:35 +0200

    Ajout de flutterbrony

---
+++ membres/flutterbrony.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Flutterbrony";
+$gravatar="958641c95e612183a7e234604f223064";
+$prenom="Antoine";
+$nom="Martinez";
+$naissance="03/02/1997";
+$url="
+";
+$bio="Brony depuis novembre 2013, je suis un passionné d' informatique qui apprends le C, C++, Python et fait aussi des réparations en tous genre a l'occasion. Toujours au lycée, je ne pratique aucune activité en particulier et ne sociabilise pas facilement non plus. Ah et je gère un serveur de <a href='http://www.legendsofequestria.com' target='_blank'>Legend of Equestria</a> ^^";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
