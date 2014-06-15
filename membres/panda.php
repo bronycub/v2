3018cf810ad65853c20edcc70e39a94845fff5d1
pandanonyme <gaspardpeyo@gmail.com>
2014-06-16 00:39:36 +0200

    Update panda.php
    Voilà j'ai mis mon gravatar, supprimé mon G+ pck privay et ajouté un Rarity à mes poneys préférés pck elle est cute quand même

--- membres/panda.php
+++ membres/panda.php
@@ -1,14 +1,11 @@
 <?php //INFOS DU MEMBRES
 //////////////////////////////////////////////
 $pseudo="Panda";
-$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$gravatar="da6d9e31177bedbe5c6b9c69e231a8e6";
 $prenom="Gaspard";
 $nom="&nbsp;";
 $naissance="30/08";
-$url="
-<a href='https://plus.google.com/110987299577934372517/posts' target='_blank' class='zocial icon googleplus'></a>
-";
+$url="";
 $bio="Je suis une bande de mec sympas, vice marrant hilarant, écrivain raté, artiste incompris, mec de l'ombre, codeur feignant, panda du futur, écoute et regarde des trucs bizarres, otaku à 42%, amateur de câlins et de yuris.";
-$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>";
-//////////////////////////////////////////////
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>&nbsp;&nbsp;<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rariy is most fabulous pony !</span>";//////////////////////////////////////////////
 include "template_membres.php"; ?>
