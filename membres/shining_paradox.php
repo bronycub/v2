e5b8967ed532c4e9378d92b59db89699632f6cc9
heuzef <contact@heuzef.com>
2014-06-19 18:07:52 +0200

    nettoyage du dossier img ponies

--- membres/shining_paradox.php
+++ membres/shining_paradox.php
@@ -9,7 +9,7 @@ $url="
 ";
 $bio="Amateur d’arts, grand lecteur, fan incontesté du cinéma d’animation mais aussi féru de nouvelles technologies, Grégoire a laissé la région bordelaise pour s’adonner aux joies d’une mathsup en région parisienne.
 Pour lui MLP est une nouvelle façon de voir le monde, complètement décalée mais si douce ! Il ne perd jamais son sourire et cherche constamment de nouvelles expériences. Serez-vous de la partie ?";
-$pony="<img src='img/ponies/11.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Daring Do is best pony!</span>&nbsp;&nbsp;<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best pony !</span>";
+$pony="<img src='img/ponies/daring_doo.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Daring Do is best pony!</span>&nbsp;&nbsp;<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
 
