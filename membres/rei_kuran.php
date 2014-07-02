111a4b77bf5a8b289e321a68c34a7a956d7f0656
heuzef <contact@heuzef.com>
2014-07-02 15:19:24 +0200

    ajout de rei kuran

---
+++ membres/rei_kuran.php
@@ -0,0 +1,21 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Rei&nbsp;Kuran";
+$gravatar="d943e923a4018c01e0584310875beff0";
+$prenom="Marion";
+$nom="Solé";
+$naissance="09/12/1993";
+$url="
+<a href='https://plus.google.com/100334898206198409114/posts' target='_blank' class='zocial icon googleplus'></a>
+<a href='https://twitter.com/ReiKuran1' target='_blank' class='zocial icon twitter'></a>
+<a href='https://www.facebook.com/rei.kuran.1' target='_blank' class='zocial icon facebook'></a>
+<a href='https://www.facebook.com/monde.cruel.rei' target='_blank' class='zocial icon facebook'></a>
+<a href='https://www.facebook.com/ReiKuranCosplay' target='_blank' class='zocial icon facebook'></a>
+<a href='http://journal-doll.blogspot.fr' target='_blank' class='zocial icon blogger'></a>
+<a href='http://www.reikuranmodelphoto.sitew.fr' target='_blank' class='zocial icon blogger'></a>
+<a href='skype:rei.kuran15?call' target='_blank' class='zocial icon skype'></a>
+";
+$bio="Rei est folle et est souvent, même voir trop, enfantine pour son âge. Une fille de première abord timide et renfermée dans son petit monde à elle, mais toujours accompagné par son oreiller ou une peluche ! Fan de My Little Pony et surtout de Rainbow Dash même avant d'avoir regardée la série. Elle se retrouve dans se monde de couleurs, de joie et d'amis ! Adorant lire (beaucoup trop) et écrire, geekette et collectionneuse ! Passionnée du Japon, d'écriture et de lecture, otaku à ses heures perdues. Photographe en herbe ainsi que cosplayeuse débutante. Une Stitch doublée d'une Tanuki dans l'âme. Adore les JDR et les RPG.";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/fluffle_puff.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pink Fluffy Unicorn Dancing On Rainbows !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
