1031a8b518c734120a2d1bc5c9fff2178b6b5839
Adrien Sohier <adrien.sohier@art-software.fr>
2014-07-03 14:48:05 +0200

    Ajout ask.fm membre rei kuran + déplacement icones

--- membres/adriens33.php
+++ membres/adriens33.php
@@ -7,6 +7,7 @@ $nom="Sohier";
 $naissance="22/02/1992";
 $url="
 <a href='https://www.art-software.fr' target='_blank' class='zocial icon chrome'></a>
+<a href='http://ask.fm/adriens33' target='_blank' class='zocial icon appnet'></a>
 <a href='skype:adrien.s33?call' target='_blank' class='zocial icon skype'></a>
 <a href='https://github.com/adrien-s' target='_blank' class='zocial icon github'></a>
 <a href='http://git.art-software.fr' target='_blank' class='zocial icon github'></a>
@@ -14,7 +15,6 @@ $url="
 <a href='http://adriens33.deviantart.com/' target='_blank' class='zocial icon disqus'></a>
 <a href='http://www.youtube.com/user/MrArtSoftware' target='_blank' class='zocial icon youtube'>/<a>
 <a href='mailto:adrien.sohier@art-software.fr' target='_blank' class='zocial icon email'></a>
-<a href='http://ask.fm/adriens33' target='_blank' class='zocial icon appnet'></a>
 ";
 $bio="Un drôle de mix entre un geek râleur et qui aime bien faire les choses par lui même et un brony :D Question caractère, c'est une sorte de mélange entre Fluttershy et Twilight Sparkle.";
 $pony="<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/doctor_whooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Doctor Whooves is best pony !</span>";
