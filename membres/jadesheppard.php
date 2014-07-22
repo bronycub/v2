0041c5e3cde5f57209c36d4dd524550dc9f54c56
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:23:11 +0200

    Create jadesheppard.php

---
+++ membres/jadesheppard.php
@@ -0,0 +1,16 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Jade Sheppard";
+$gravatar="8562a078e26f318a1ccdaef6824a7329";
+$prenom="Christine";
+$nom="Denjean";
+$naissance="30/06/1986";
+$url="
+<a href='http://jadesheppard.tumblr.com/' target='_blank' class='zocial icon tumblr'></a>
+";
+$bio="J'ai découvert MLP:FiM un peu par hasard, mais on peut dire que j'ai accroché de suite, même si au départ avec mon conjoint nous le regardions en vf, nous avons fini par le regarder en VO, puis récemment la version japonaise.
+
+Dans les mane 6 je n'ai pas à proprement dit de favorite. Ce qui est assez cocasse c'est qu'en temps que fille, j'ai eu le droit à la G1 et malgré tout je n'ai eu qu'un seul poney de cette époque, que j'ai d'ailleurs ré-acquit récemment. J'ai quelques goodies, mais je trouve pas assez d'items sur big macintosh, ni pour les pegasis.";
+$pony="<img src='img/ponies/big_macintosh.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Big Macintosh is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
