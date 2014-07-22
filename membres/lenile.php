552535bace0d387196d30b02ecceebd4392a1b9d
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:28:35 +0200

    Create lenile.php

---
+++ membres/lenile.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Lenile Sharm";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Raphaël";
+$nom="Chammah";
+$naissance="06/10/1995";
+$url="
+";
+$bio="Je suis Raphaël, vous pouvez aussi m'appeler "Lenile". Actuellement je suis rédacteur cher Le Poney Blanc un tres grand blog de Brony.";
+$pony="<img src='img/ponies/applejack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Applejack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
