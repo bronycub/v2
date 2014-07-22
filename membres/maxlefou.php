6392e8f576132f9f39234ccc1279df283428f659
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:09:39 +0200

    correction typo

--- membres/maxlefou.php
+++ membres/maxlefou.php
@@ -12,7 +12,7 @@ $url="
 <a href='http://twitter.com/maxlefou' target='_blank' class='zocial icon twitter'></a>
 <a href='http://www.maxlefou.com' target='_blank' class='zocial icon chrome'></a>
 ";
-$bio="Artiste parodiste et polivalent, devenu brony en 2012 ... J'ai bossé avec MLPSubtitles pour la Saison 4, fait un RPG My Little Pony, un album de parodies sur My Little Pony, collectionne les peluches, les figurines, les jeux,... Bref, un obsédé total des poneys...
+$bio="Artiste parodiste et polyvalent, devenu brony en 2012 ... J'ai bossé avec MLPSubtitles pour la Saison 4, fait un RPG My Little Pony, un album de parodies sur My Little Pony, collectionne les peluches, les figurines, les jeux,... Bref, un obsédé total des poneys...
 
 En dehors de ça, je fais des parodies de chansons depuis plus de 10 ans, sait faire du graphisme, coder sur Ren'Py et aime bien manipuler les sons les la musique. Fiancé à Jade Sheppard et vivant avec elle depuis plus de 10 ans.";
 $pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best unicorn !</span>&nbsp;&nbsp;<img src='img/ponies/sweetie_belle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Sweetie Belle is best CMC !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is a muffin !</span>";
