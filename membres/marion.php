792a2e9f215ea26e64ffe0681469cd120ffce829
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:36:07 +0200

    Create marion.php
    N'a pas fourni son pseudo (a mis son n° de tel à la place) à voir

---
+++ membres/marion.php
@@ -0,0 +1,18 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Marion Roth";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Marion";
+$nom="Roth";
+$naissance="17/04/1995";
+$url="
+<a href='https://www.facebook.com/profile.php?id=100000253346216' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Fille simple et qui aime jouer ^^
+Petite geek amicale qui a peur des araignées, des chenilles et..bah de quasiment tous les insectes en faite >_<
+Par contre ne me proposez jaaaaaaaaaaaamais de fraise ! Ôh non ! j'adore ça ! Aucune femme, même enceinte, ne peut aimer les fraises autant que moi > 3> fuuu !
+Sinon je suis une grande timide >_<
+Alors vous moquez pas T-T me taquinez pas je rougis trop facilement T----T";
+$pony="&nbsp;";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
