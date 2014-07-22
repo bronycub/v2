c180f8d05e9b036f73a7e7e93944e6276667b1df
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:58:04 +0200

    Create ivan.php

---
+++ membres/ivan.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Ivan Alt";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Ivan";
+$nom="Alt";
+$naissance="23/08/1995";
+$url="
+";
+$bio="Brony depuis quelque mois seulement, j'avance petit à petit vers la bronyfication parfaite.
+
+Je traine beaucoup sur jvc et joue avec des potes. Je suis en PSI à Eiffel.
+
+Voilà, bonne journée à toi qui a lu ceci ;)";
+$pony="<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
