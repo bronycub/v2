520367015583801baa6289223ae21e62884274b0
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:49:59 +0200

    Gravatar added, codage corrigé

--- membres/lenile.php
+++ membres/lenile.php
@@ -1,13 +1,13 @@
 <?php //INFOS DU MEMBRES
 //////////////////////////////////////////////
 $pseudo="Lenile Sharm";
-$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$gravatar="344c5cf75607814c880a4746eb46221a";
 $prenom="Raphaël";
 $nom="Chammah";
 $naissance="06/10/1995";
 $url="
 ";
-$bio="Je suis Raphaël, vous pouvez aussi m'appeler "Lenile". Actuellement je suis rédacteur cher Le Poney Blanc un tres grand blog de Brony.";
+$bio="Je suis Raphaël, vous pouvez aussi m'appeler Lenile. Actuellement je suis rédacteur cher Le Poney Blanc un tres grand blog de Brony.";
 $pony="<img src='img/ponies/applejack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Applejack is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
