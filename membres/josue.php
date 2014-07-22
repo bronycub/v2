2eb7d5e4e6c29e7fa4dc60c1dbd54b58e3ff9188
Max le Fou <maxlefou@users.noreply.github.com>
2014-07-22 14:54:55 +0200

    Create josue.php

---
+++ membres/josue.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Josué Foucaud";
+$gravatar="56c28f57abeb9ab9fd4c43b054872ad0";
+$prenom="Josué";
+$nom="Foucaud";
+$naissance="23/03/1997";
+$url="
+<a href='https://twitter.com/Jo_The_Cube' target='_blank' class='zocial icon twitter'></a>
+";
+$bio="Brony depuis plusieurs année, Otaku, Geek, Métalleux, Rolliste, Fan de SF Assumé a 100% Et joueur de carte Magic the Gathering !Je touche vite fais au C, C++, et au Python !";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>&nbsp;&nbsp;<img src='img/ponies/derpy_hooves.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Derpy Hooves is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
