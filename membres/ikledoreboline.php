549e83e0a9447f139a11d446d5fcab06da3d210f
heuzef <contact@heuzef.com>
2014-08-01 18:51:20 +0200

    ajout du membre Ikledoreboline

---
+++ membres/ikledoreboline.php
@@ -0,0 +1,12 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Ikledoreboline";
+$gravatar="d41d8cd98f00b204e9800998ecf8427e";
+$prenom="Etienne";
+$nom="Cadillon";
+$naissance="18/04/1998";
+$url="";
+$bio="Je m'appelle Etienne, actuellement lycéen en Système Electronique Numérique, je suis un passionné de jeux vidéos depuis tout petit, il y a peu de chose touchant l'ordinateur qui ne m'intéresse pas. J'ai regardé MLP par hasard grâce à une amie qui m'à fait découvrir un monde totalement différent que ceux des animes et mangas que je puisse lire ou voir et se fus parfaits !";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
