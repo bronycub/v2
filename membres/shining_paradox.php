a701ba863ab9999d616d9b90273ae63b6f7b79ca
heuzef <contact@heuzef.com>
2014-08-16 10:23:00 +0200

    MAJ Shining Paradox

--- membres/shining_paradox.php
+++ membres/shining_paradox.php
@@ -1,15 +1,15 @@
 <?php //INFOS DU MEMBRES (Shining Paradox est le plus beau des poneys, je l'aime beaucoup - Un admirateur secret)
 //////////////////////////////////////////////
 $pseudo="Shining&nbsp;Paradox";
-$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$gravatar="560550c9f6d424891ea34a8b69bc9f23";
 $prenom="Grégoire";
 $nom="missing";
-$naissance="mai 1995";
+$naissance="05/1995";
 $url="<a href='http://www.shiningparadox.fr/' target='_blank' class='zocial icon chrome'></a>
 <a href='https://twitter.com/ShiningParadox' target='_blank' class='zocial icon twitter'></a>
 <a href='http://shiningparadox.deviantart.com/' target='_blank' class='zocial icon disqus'></a>
 <a href='https://www.youtube.com/ShiningParadox' target='_blank' class='zocial icon youtube'></a>";
-$bio="Étudiant en mathématiques spéciales, globe-trotteur, journaliste à ses heures, administrateur pour Community Network, fondateur du Direct Sims et de MLPFictions.";
+$bio="Étudiant en mathématiques spéciales, globe-trotteur, journaliste à ses heures, administrateur pour Community Network, fondateur du Direct Sims et de MLPFictions. Pour lui MLP est une nouvelle façon de voir le monde, complètement décalée mais si douce ! Il ne perd jamais son sourire et cherche constamment de nouvelles expériences.";
 $pony="<img src='img/ponies/daring_do.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Daring Do is best pony!</span>&nbsp;&nbsp;<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
