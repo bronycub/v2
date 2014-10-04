836436992bbe3bd51695fe262b94b8d1c33a5635
heuzef <contact@heuzef.com>
2014-10-04 17:21:20 +0200

    ajout membre Makhai

---
+++ membres/makhai.php
@@ -0,0 +1,24 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Makhai";
+$gravatar="38d3b3d8ba1fb606d459569f46936f6c";
+$prenom="Clément";
+$nom="Calliau";
+$naissance="11/11/1994";
+$url="
+<a href='http://makhai.fr' target='_blank' class='zocial icon chrome'></a>
+<a href='https://www.facebook.com/Makhai.0' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Je m'appelle Clément, je suis en première STI et je passe en teminale.<br />
+J'ai été initié par des brony de ma classe mais je connaissait déjà d'autres membres comme Rei Kuran.<br />
+<br />
+J'aime :<br />
+- Lire : Eragon (beurk le film), Ewilan, Naheubeuk (après l'audio bien sur), et trop de livres pour une si petite zone de texte ^^'.<br />
+- Jouer : AoE2, Wesnoth, S4league, tera, Pokemon gemme et d'autres que j'oublie.<br />
+- Regarder : MLP bien sur, mais aussi plein d'animé ou séries comme Stargate, GoT, No game no life, SAO (enfiiin!) et pleiiin d'autres.<br />
+- Ecouter : Nightwish, Rammstein, Shaka Ponk, Ska-p, Two steps from hell, ...<br />
+<br />
+PS : J'aime le Nutella. Beaucoup.";
+$pony="<img src='img/ponies/scootaloo.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Scootaloo is best pony !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
