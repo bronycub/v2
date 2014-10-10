4bad2de845746ba9fdcc8259144b2de04f05a61a
heuzef <contact@heuzef.com>
2014-10-10 18:08:43 +0200

    ajout de Razorbill

---
+++ membres/razorbill.php
@@ -0,0 +1,16 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Razorbill";
+$gravatar="b1a2f006f790c5935e1814528adefe3a";
+$prenom="Clara";
+$nom="Ziegelmeyer";
+$naissance="22/09/1998";
+$url="
+";
+$bio="Je suis gentille, et gentille, et 4.5.<br />
+Je suis une patate violette, une licorne, un pingouin, un mouton noir, une princesse, un chat, un vampire.<br />
+Euh j'ai découvert MLP parce que quand on va sur des forums, on trouve des gens chelous. J'ai donc regardé le premier épisode pour voir, sauf qu'il était en deux parties, donc deuxième épisode, puis un troisième pour voir comment ils continuent l'histoire, puis la saison pour voir en général, puis je me suis dit qu'en fait c'était trop bien, et je suis accro.<br />
+Je suis une vive critiqueuse de Equestria Girls (qui est sorti peu après que j'ai commencé la saison 2).<br />Bouaaaaark!";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
