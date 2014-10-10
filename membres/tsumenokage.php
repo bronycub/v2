fcf006571667e2b11291dd95e45d7f2d48219632
heuzef <contact@heuzef.com>
2014-10-10 16:17:33 +0200

    correction tsumenokage.php

--- membres/tsumenokage.php
+++ membres/tsumenokage.php
@@ -8,7 +8,7 @@ $naissance="28/01/1993";
 $url="
 <a href='https://www.facebook.com/Tsumenokage' target='_blank' class='zocial icon facebook'></a>
 ";
-$bio="Salut à tous, je suis Jérémy et je suis brony depuis début 2014 grâce à un très bon ami qui m'a fait découvrir MLP.  Je suis étudiant en informatique, geek, grand mélomane et pas mal d'autre chose. Je joue énormément au jeu de rôle (à la fois en tant que joueur et MJ) donc si certains d'entre vous sont intéressé pourquoi pas se retrouver en brony pour jouer ? j'ai rencontré pas mal de monde à l'édition 2014 d'animasia qui m'ont parlé de ce regroupement bordelais (merci Rei Kuran et Lenile Sharm entre autre). En espérant passer de futurs bons moments avec tout le monde :)";
-$pony="img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp";
+$bio="Salut à tous ! Je suis Jérémy et je suis brony depuis début 2014 grâce à un très bon ami qui m'a fait découvrir MLP.  Je suis étudiant en informatique, geek, grand mélomane et pas mal d'autre chose. Je joue énormément au jeu de rôle (à la fois en tant que joueur et MJ) donc si certains d'entre vous sont intéressé pourquoi pas se retrouver en brony pour jouer ?<br /> J'ai rencontré pas mal de monde à l'édition 2014 d'animasia qui m'ont parlé de ce regroupement Bordelais (merci Rei Kuran et Lenile Sharm entre autre).<br />En espérant passer de futurs bons moments avec tout le monde :)";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
