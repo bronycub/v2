6fff2d9601f42f4c77cc146cc64bd66446fac485
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2014-11-09 12:51:29 +0100

    Ajout de DarkNight5047

---
+++ membres/DarkNight5047.php
@@ -0,0 +1,12 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="DarkNight5047";
+$gravatar="553e850bb01e811fba5c36a9caa24279";
+$prenom="Piquet";
+$nom="Nathanael";
+$naissance="09/09/1999";
+$url="";
+$bio="Salut à tous ! Donc je m'appel Nathanael et j'ai 15 ans. Je suis un grand fan de mlp et pour info je suis déjà aller dans une convention brony :D (3PS) Voilà.";
+$pony="<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
