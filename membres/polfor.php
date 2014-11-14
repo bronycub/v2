fa8fc4413b9e92e2756207f844abfd7acac9b13c
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2014-11-14 14:52:29 +0100

    Ajout première info de Polfor

---
+++ membres/polfor.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Polfor";
+$gravatar="";
+$prenom="Paul";
+$nom="Forsans";
+$naissance="25/09/2000";
+$url="
+";
+$bio="Un petit brony débutant...";
+$pony="<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
