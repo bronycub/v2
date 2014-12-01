e19402671a2b1619b6d799e10536cf902a0eefc1
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2014-12-01 13:47:13 +0100

    Ajout de NightFire

---
+++ membres/nightfire.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Nightfire";
+$gravatar="";
+$prenom="Adrien";
+$nom="Coupet";
+$naissance="21/12/1991";
+$url="<a href='http://stashine-nightfire.deviantart.com/' target='_blank' class='zocial icon disqus'></a>
+";
+$bio="Brony depuis 1 ans, je dessine et je sais pas quoi dire d’autre * squee * ";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
