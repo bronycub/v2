a44a5d28c0f886667a3cbec79dab247c1122846a
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2014-12-01 13:52:02 +0100

    Ajout de Spokeek

---
+++ membres/spokeek.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Spokeek";
+$gravatar="";
+$prenom="Alexandre";
+$nom="Belhomme";
+$naissance="02/12/1995";
+$url="";
+$bio="Etudiant en ingÃ©nierie informatique, passionnÃ© par la culture communautaire internet et brony.
+Tout est dit ! ";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
