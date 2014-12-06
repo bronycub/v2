dad36284da1e44d281e21857f3ccf830d1dc77fa
heuzef <contact@heuzef.com>
2014-12-06 16:01:23 +0100

    ajout snowydreamn

---
+++ membres/snowy_dreamn.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="SnowyDreamn";
+$gravatar="dc5d282634cca1e8580c4c817e122aae";
+$prenom="Alix";
+$nom="Dubos";
+$naissance="23/01/1998";
+$url="<a href='http://thesnowydream.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='http://thesnowydream.tumblr.com' target='_blank' class='zocial icon tumblr'></a>";
+$bio="J'aime BEAUCOUP dessiner des pony ! J'ai même fait le mien, BloodyWater.
+Je peux paraître étrange pas moment, mais bon si on est gentil je suis gentille ^^.
+N'hésitez pas à vister mon <a href='http://thesnowydream.deviantart.com' target='_blank'>DeviantArt</a> et mon <a href='http://thesnowydream.tumblr.com' target='_blank'>Tumblr</a> pour voir mes créations !";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
