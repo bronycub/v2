c264fd03dabc378dc6cd69e054cbed581d4022a6
heuzef <contact@heuzef.com>
2015-01-05 16:00:11 +0100

    correction fuzzion33

--- membres/fuzzion33.php
+++ membres/fuzzion33.php
@@ -6,10 +6,10 @@ $prenom="Esteban";
 $nom="Gonzalez";
 $naissance="18/03/1999";
 $url="";
-$bio="Ok alors voila le blabla : 
-Je suis un gamer, brony, humain, et surtout affamé. C'est grâce a un ami brony que j'ai découvert le site et grâce a mon frère que 
-j'ai aimer les poney. j'aime beaucoup la musique. Mais surtout les jeux comme (Call of Duty, minecraft ou autres dans le même style). 
-Voila je c'est plus quoi dire donc j’arrête de parler dans 3. . 2. . 1. . 0. ";
+$bio="Ok alors voila le blabla :
+Je suis un gamer, brony, humain, et surtout affamé. C'est grâce a un ami brony que j'ai découvert le site et grâce a mon frère que
+j'ai aimé les poney. J'aime beaucoup la musique, mais surtout les jeux-vidéo (Call of Duty, Minecraft ou autres dans le même style).
+Voila, je ne sais plus quoi dire, donc j’arrête de parler dans 3, 2, 1, 0.";
 $pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
