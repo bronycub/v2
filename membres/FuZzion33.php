2429dcb496ea31098e6c831906d2700e667c11c6
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-01-05 11:01:34 +0100

    Ajout de Fuzzion33

---
+++ membres/FuZzion33.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="FuZzion33";
+$gravatar="0d25aaf75939b9d136e57a0614c09118";
+$prenom="Esteban";
+$nom="Gonzalez";
+$naissance="18/03/1999";
+$url="";
+$bio="Ok alors voila le blabla : 
+Je suis un gamer, brony, humain, et surtout affamé. C'est grâce a un ami brony que j'ai découvert le site et grâce a mon frère que 
+j'ai aimer les poney. j'aime beaucoup la musique. Mais surtout les jeux comme (Call of Duty, minecraft ou autres dans le même style). 
+Voila je c'est plus quoi dire donc j’arrête de parler dans 3. . 2. . 1. . 0. ";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best pony !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
