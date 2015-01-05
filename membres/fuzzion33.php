e465715094ccc92c5c7b2993aedec24512c268b6
heuzef <contact@heuzef.com>
2015-01-05 16:15:20 +0100

    maj fuzzion33

--- membres/fuzzion33.php
+++ membres/fuzzion33.php
@@ -5,7 +5,10 @@ $gravatar="0d25aaf75939b9d136e57a0614c09118";
 $prenom="Esteban";
 $nom="Gonzalez";
 $naissance="18/03/1999";
-$url="";
+$url="
+<a href='https://www.facebook.com/esteban.gonzalez.1610' target='_blank' class='zocial icon facebook'></a>
+<a href='skype:vortex634?call' target='_blank' class='zocial icon skype'></a>
+";
 $bio="Ok alors voila le blabla :
 Je suis un gamer, brony, humain, et surtout affamé. C'est grâce a un ami brony que j'ai découvert le site et grâce a mon frère que
 j'ai aimé les poney. J'aime beaucoup la musique, mais surtout les jeux-vidéo (Call of Duty, Minecraft ou autres dans le même style).
