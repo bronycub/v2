acd089f59b61c113ee57578d9d448913f1205273
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-01-16 12:28:17 +0100

    Ajout d'Elvon_Ninthnight

---
+++ membres/Elvon_Ninthnight.php
@@ -0,0 +1,20 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Elvon Ninthnight";
+$gravatar="1ded90863fcf34a5f8236630a10a56ab";
+$prenom="Thomas";
+$nom="Belguiral";
+$naissance="07/11/1994";
+$url="";
+$bio="Avant tout musicien (Guitare classique/électrique, batterie, chant) , je ne suis brony que depuis environ 3 mois (je remerci donc Heuzef pour cette élan d’amabilité 
+lors d’une rencontre non Bronyesque, ce qui m'a permis par la suite de passer deux ou trois nuit nuit blanche sur les saisons 1/2/3 de MLP)
+Créatif, je me balade souvent avec mon araignée au plafond (cela reste une expression) si vous croisai un type qui sifflote ou entonne un air sans plus de raisons veuillez ne pas paniquer X3 
+(avoir un Orchestre complet dans sa caboche ce n'est pas toujours évident mais ça reste cool quand on essaie de nouvelles impros) 
+Je suis aussi G33K (Jeux solo/coop pc avant tout, et puis multijoueur-compétitif entre potes (Tit’s an fall/garry’s mod/lol, mais aussi multijoueur console de salon parce que mariokart/ 
+marioparty/ SSB. et Halo c’est le must du fun quand on est 4 sur la tv) mais aussi Réaliste (Je suis synonyme de Shcoumoun je suis la partie basse du dès a vingt faces, et les 7% de chance de mourir bétement)
+J’aime les mangas (Vidéo/Bouquin) je suis ouvert (on ne juge pas un livre à sa couverture) et puis le reste à vous de le découvrir, a très bientôt Everyponies !
+
+(pour ce qui joue à Minecraft si vous connaissez un serveur nommé Poneycub, j’était la licorne du staff de 2012 à fin Juillet 2014)";
+$pony="<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>&nbsp;&nbsp;<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
