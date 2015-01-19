88b6f8fa178717a567f0ba80df4e9cc59f4ec8f4
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-01-19 12:22:37 +0100

    Correction Réaliste -> Rôliste

--- membres/Elvon_Ninthnight.php
+++ membres/Elvon_Ninthnight.php
@@ -14,7 +14,7 @@ lors d’une rencontre non Bronyesque, ce qui m'a permis par la suite de passer 
 Créatif, je me balade souvent avec mon araignée au plafond (cela reste une expression) si vous croisai un type qui sifflote ou entonne un air sans plus de raisons veuillez ne pas paniquer X3 
 (avoir un Orchestre complet dans sa caboche ce n'est pas toujours évident mais ça reste cool quand on essaie de nouvelles impros) 
 Je suis aussi G33K (Jeux solo/coop pc avant tout, et puis multijoueur-compétitif entre potes (Tit’s an fall/garry’s mod/lol, mais aussi multijoueur console de salon parce que mariokart/ 
-marioparty/ SSB. et Halo c’est le must du fun quand on est 4 sur la tv) mais aussi Réaliste (Je suis synonyme de Shcoumoun je suis la partie basse du dès a vingt faces, et les 7% de chance de mourir bétement)
+marioparty/ SSB. et Halo c’est le must du fun quand on est 4 sur la tv) mais aussi Rôliste (Je suis synonyme de Shcoumoun je suis la partie basse du dès a vingt faces, et les 7% de chance de mourir bétement)
 J’aime les mangas (Vidéo/Bouquin) je suis ouvert (on ne juge pas un livre à sa couverture) et puis le reste à vous de le découvrir, a très bientôt Everyponies !
 
 (pour ce qui joue à Minecraft si vous connaissez un serveur nommé Poneycub, j’était la licorne du staff de 2012 à fin Juillet 2014)";
