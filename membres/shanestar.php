4aca5753f120bd39c284e671353109fc4fa74881
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-01-31 14:51:39 +0100

    Ajout de ShaneStar

---
+++ membres/shanestar.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="ShaneStar";
+$gravatar="b6503c55f63ab5b9a00412b76fac7d3e";
+$prenom="Florian";
+$nom="B.";
+$naissance="07/03/1996";
+$url="
+";
+$bio="Hellow à tous !
+Alors voilà j’me présente, Florian, 18 ans, Lycéen flemmard et fan de culture Japonaise ainsi que de MLP depuis peu.
+Otaku, amateur de Rock et fan de jeux vidéos (RPG et de Combat), j’en suis encore à mes bégaiements en JdR et j’apprécie toujours une bonne sieste. =w= ";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
