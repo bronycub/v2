a8d91d0d8acd2255a20f5359fabd0ec217665132
heuzef <contact@heuzef.com>
2015-02-10 17:19:15 +0100

    fix aragox

--- membres/aragox.php
+++ membres/aragox.php
@@ -8,9 +8,9 @@ $naissance="16/11/1994";
 $url="
 <a href='http://aragox.deviantart.com' target='_blank' class='zocial icon disqus'></a>
 ";
-$bio="Salut ! moi c'est Aragox, Je suis un Gamers vétéran, Digifan depuis ma naissance et Brony depuis début 2014 . Je suis quelqu'un d'assez extraverti, qui aime rencontrer des gens,
- aller au bar et ne plus se souvenir de ces même gens jusqu'a ce qu'il me raconte que j'ai imiter une carotte volante toute la soirée... Mon Pony préférer est Applejack et j'attache une petite 
- importance à octavia. A que cela ne tienne je suis prêt à vous recevoir bande de pony fou !";
-$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>";
+$bio="Salut ! moi c'est Aragox, Je suis un Gamers vétéran, Digifan depuis ma naissance et Brony depuis début 2014. Je suis quelqu'un d'assez extraverti, qui aime rencontrer des gens,
+ aller au bar et ne plus se souvenir de ces même gens jusqu'a ce qu'il me raconte que j'ai imiter une carotte volante toute la soirée ... Mon Pony préférer est Applejack et j'attache une petite
+ importance à Octavia. A que cela ne tienne je suis prêt à vous recevoir bande de pony fou !";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/octavia.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Octavia is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
