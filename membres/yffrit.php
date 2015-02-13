56f728e12f13abaf92a803d3990457e4b752fd0a
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-02-13 22:53:57 +0100

    Ajout de Yffrit

---
+++ membres/yffrit.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Yffrit";
+$gravatar="1f0c6de5fc4bc81cd2197cea80998829";
+$prenom="Thomas";
+$nom="Becker";
+$naissance="26/07/1990";
+$url="
+";
+$bio="Hello amis Bronies ! Moi c'est Thomas. Brony à mes heures perdues, Otaku et gamer à temps plein. Je suis administrateur réseau de carrière et bientôt (peut être) à la tête du prochain studio de développement de jeux vidéo de Bordeaux ! croisons les doigts";
+$pony="<img src='img/ponies/cadence.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Cadence is best princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
