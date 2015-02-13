c04e25dd60dd4f15b4edb7fecb88c501258fcb60
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-02-13 22:53:32 +0100

    Ajout de Starbook

---
+++ membres/starbook.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="StarBook";
+$gravatar="25b12e9a554dd7e1e3bc03c1c998d32f";
+$prenom="Lucy";
+$nom="Alves";
+$naissance="16/12/2001";
+$url="
+<a href='http://xtwilightsparkle.skyrock.com/' target='_blank' class='zocial icon blogger'></a>
+";
+$bio="Petite Pegasister nouvelle dans le monde de My Little Pony, collégienne au niveau quatrième, partage ses passions avec sa meilleure amie, idolâtre Twilight Sparkle & Luna.";
+$pony="<img src='img/ponies/Luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best princess !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
