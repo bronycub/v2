4b83907c9a9e54968d222e0943c91439d1517b96
heuzef <contact@heuzef.com>
2015-02-15 18:29:20 +0100

    correction yffrit et starbook

--- membres/starbook.php
+++ membres/starbook.php
@@ -9,6 +9,6 @@ $url="
 <a href='http://xtwilightsparkle.skyrock.com/' target='_blank' class='zocial icon blogger'></a>
 ";
 $bio="Petite Pegasister nouvelle dans le monde de My Little Pony, collégienne au niveau quatrième, partage ses passions avec sa meilleure amie, idolâtre Twilight Sparkle & Luna.";
-$pony="<img src='img/ponies/Luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best princess !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>";
+$pony="<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best princess !</span>&nbsp;&nbsp;&nbsp;<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
--- membres/yffrit.php
+++ membres/yffrit.php
@@ -7,7 +7,7 @@ $nom="Becker";
 $naissance="26/07/1990";
 $url="
 ";
-$bio="Hello amis Bronies ! Moi c'est Thomas. Brony à mes heures perdues, Otaku et gamer à temps plein. Je suis administrateur réseau de carrière et bientôt (peut être) à la tête du prochain studio de développement de jeux vidéo de Bordeaux ! croisons les doigts";
+$bio="Hello amis Bronies ! Moi c'est Thomas. Brony à mes heures perdues, Otaku et gamer à temps plein. Je suis administrateur réseau de carrière et bientôt (peut être) à la tête du prochain studio de développement de jeux vidéo de Bordeaux ! Croisons les doigts !";
 $pony="<img src='img/ponies/cadence.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Cadence is best princess !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
