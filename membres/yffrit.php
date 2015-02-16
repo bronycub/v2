0c2e2526748720f49023c78c0f52bc2b1f1bbf39
heuzef <contact@heuzef.com>
2015-02-16 08:56:53 +0100

    update Yffrit

--- membres/yffrit.php
+++ membres/yffrit.php
@@ -7,7 +7,7 @@ $nom="Becker";
 $naissance="26/07/1990";
 $url="
 ";
-$bio="Hello amis Bronies ! Moi c'est Thomas. Brony à mes heures perdues, Otaku et gamer à temps plein. Je suis administrateur réseau de carrière et bientôt (peut être) à la tête du prochain studio de développement de jeux vidéo de Bordeaux ! Croisons les doigts !";
+$bio="Hello amis Bronies ! Moi c’est Thomas ! Brony à mes heures perdues depuis peu, Otaku et gamer à temps plein. Je suis aussi bénévole depuis presque 3 ans à l'association Mandora (Animasia yeah !) et franchement je ne me plains pas. Je suis administrateur réseau de carrière et bientôt (peut être) à la tête du prochain studio de développement de Bordeaux ! *croise les doigts*";
 $pony="<img src='img/ponies/cadence.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Cadence is best princess !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
