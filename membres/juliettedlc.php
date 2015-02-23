b29102f42edb5cbe9777da17c3c8a4da8923a4d2
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-02-23 16:00:57 +0100

    Ajout de julietteDLC à completer

---
+++ membres/juliettedlc.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="JulietteDlc";
+$gravatar="4b3622a9ff6b2c9b1c22db3f8807283b";
+$prenom="Juliette";
+$nom="De la Châtre";
+$naissance="27/06/1998";
+$url="
+";
+$bio=":)";
+$pony="<img src='img/ponies/fluffle_puff.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pink Fluffy Unicorn Dancing On Rainbows !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
