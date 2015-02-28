50dfaa31a403a6ea4f80420d3e3db37828add33f
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-02-28 21:19:13 +0100

    Ajout negouille

---
+++ membres/negouille.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Negouille";
+$gravatar="4b9d96a9f533b629022433ce44a4c696";
+$prenom="Lucas";
+$nom="Briosca";
+$naissance="10/10/1999";
+$url="<a href='https://twitter.com/NegaV3' target='_blank' class='zocial icon twitter'></a>
+";
+$bio="Je suis un gobelin bouré. J'adore faire de la modélisation 3D, les chocobos, les dessins, je suis un otaku.
+J'ai hâte de faire des évents et agir dans l'humanitaire avec vous tous ^^";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
