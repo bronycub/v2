e5c165f42592d3a488632461c2c7a3877cab8f79
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-02-28 21:18:47 +0100

    Modification bio/lien

--- membres/juliettedlc.php
+++ membres/juliettedlc.php
@@ -6,8 +6,10 @@ $prenom="Juliette";
 $nom="De la Châtre";
 $naissance="27/06/1998";
 $url="
+<a href='https://www.facebook.com/juliette.dlc.F' target='_blank' class='zocial icon facebook'></a>
 ";
-$bio=":)";
+$bio="Coucou! je m'appelle Juliette, je suis une passionnée d'histoire principalement de la seconde guerre mondiale.
+Je suis aussi une grande fan de la reine des neiges! Je suis devenue brony par le plus grand des hasards lors d'une soirée ou on m'en à parlée et après des recherches j'ai de suite accrochée!";
 $pony="<img src='img/ponies/fluffle_puff.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pink Fluffy Unicorn Dancing On Rainbows !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
