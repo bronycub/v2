d72bf0ffdd4f0cee4432e41a1efd6b54dcc397c6
heuzef <contact@heuzef.com>
2015-03-03 10:42:10 +0100

    ajout de 3 membres

--- membres/fidzu.php
+++ membres/fidzu.php
@@ -8,16 +8,16 @@ $naissance="23/09/1997";
 $url="
 <a href='http://fidzu.deviantart.com/' target='_blank' class='zocial icon disqus'></a>
 <a href='https://www.youtube.com/channel/UC6eYJf28sRUZnShPxrtWY_Q' target='_blank' class='zocial icon youtube'></a>
-<a href='https://www.art-software.fr' target='_blank' class='zocial icon soundcloud'></a>
+<a href='https://soundcloud.com/julienhoang ' target='_blank' class='zocial icon soundcloud'></a>
 ";
-$bio="Moi c'est Julien, ou Fidzu sur la toile. Je suis bordelais depuis toujours, 
-du moins j'habite a villenave d'ornon. En ce moment, j’étudie au CFA Gustave Eiffel 
+$bio="Moi c'est Julien, ou Fidzu sur la toile. Je suis bordelais depuis toujours,
+du moins j'habite a villenave d'ornon. En ce moment, j’étudie au CFA Gustave Eiffel
 (pas loin de la Victoire)
-Je suis devenu Brony depuis début 2014. Tout a commencé un soir de pleine lune, la nuit 
-brillait de mille étoiles... non plus sérieusement, je cherchais un tuto sur youtube et 
-la musique de fond était une musique de SimGretina ce qui m'a conduit a faire des 
-recherches sur MLP et vous connaissez la suite. Franchement, c'est l'une des meilleurs 
-choses qui me soit arrivées. Grace a cette série et a cette Merveilleuse communauté, 
+Je suis devenu Brony depuis début 2014. Tout a commencé un soir de pleine lune, la nuit
+brillait de mille étoiles... non plus sérieusement, je cherchais un tuto sur youtube et
+la musique de fond était une musique de SimGretina ce qui m'a conduit a faire des
+recherches sur MLP et vous connaissez la suite. Franchement, c'est l'une des meilleurs
+choses qui me soit arrivées. Grace a cette série et a cette Merveilleuse communauté,
 je me suis trouvé une passion dans la musique, la sculpture et le dessin digital.";
 $pony="<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>";
 //////////////////////////////////////////////
--- membres/negouille.php
+++ membres/negouille.php
@@ -7,7 +7,7 @@ $nom="Briosca";
 $naissance="10/10/1999";
 $url="<a href='https://twitter.com/NegaV3' target='_blank' class='zocial icon twitter'></a>
 ";
-$bio="Je suis un gobelin bouré. J'adore faire de la modélisation 3D, les chocobos, les dessins, je suis un otaku.
+$bio="Je suis un gobelin bouré. Je me passione pour la modélisation 3D, les chocobos, les dessins, je suis un otaku. J’adore MLP et j’aime bien le popcorn. Je suis un petit poussin a la recherche de l’aventure, blagueur, intellectuel et toujours a l’écoute.<br />
 J'ai hâte de faire des évents et agir dans l'humanitaire avec vous tous ^^";
 $pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>";
 //////////////////////////////////////////////
