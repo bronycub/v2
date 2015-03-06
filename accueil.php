7d7cc9f59e2d8a6dc098d91e1b96940598236bed
heuzef <contact@heuzef.com>
2015-03-06 13:39:38 +0100

    ajout contribution de valcab33

--- accueil.php
+++ accueil.php
@@ -1,8 +1,5 @@
 <?php include 'header.php'; ?>
 
-
-
-
 	<div class="container-fluid">
 		<div class="row">
 		 <div class="col-md-2">
@@ -11,16 +8,345 @@
 		 <div class="col-md-8">
 			<img src="./img/title_join_min.png" alt="logo_bronycub" class="img-responsive" />
 			<hr />
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+
+
+
+
+			<h2 style="color:#772953">Introduction</h2>
+
+			<p>Bienvenue ! Avant de procéder à votre inscription voici un petit guide d’accueil qui va vous permettre de savoir tout ce qu’il faut sur BronyCUB pour en profiter un maximum.</p>
+
+			<h3>L'histoire de BronyCUB</h3>
+
+			<p>BronyCUB est un collectif et non une association, ceci pour éviter l’aspect administratif n’ayant aucun rapport avec nos liens d'amitié, mais également pour que l'intégration des nouveaux soit simple, rapide et gratuite.</p>
+
+
+
+			<p>Absolument tout le monde est le bienvenu sous 2 conditions : habiter la Communauté Urbaine de Bordeaux (ou au moins y être actif assez souvent) et être un Brony (ou au moins en partager les valeurs tel que l’amitié et le respect).
+
+				Si vous n’habitez pas dans le coin, nous vous invitons à rejoindre le collectif brony de votre région : <a href="http://carte.leponeyblanc.fr" target="_blank">carte.leponeyblanc.fr</a> </p>
+
+
+
+				<p>BronyCUB a été fondé par Heuzef en 2013 avec l’espoir de réunir de formidables “geeks” qui souhaitent établir des liens fort entre eux (au delà du “pote” très répendu de nos jours) tout en agissant dans l'humanitaire. L’objectif est donc du “fun” et un maximum de partage de celui-ci pour un accomplissement de soi.</p>
+
+				<blockquote class="pull-right">
+
+					<p>Le bonheur est contagieux : entourez-vous de visages heureux.</p>
+
+					<small>Dominique Glocheux</small>
+
+				</blockquote>
+
+				<br />
+
+				<h3>QG</h3>
+
+				<p>BronyCUB étant un collectif, nous ne disposons pas de locaux. Mais nous avons tout de même un “Quartier Général” ou nous nous retrouvons assez souvent : le Calice du dragon. Plus d’infos ici : <a href="http://bronycub.org/qg.php" target="_blank">QG</a>.</p>
+
+
+
+				<h3>Actions Humanitaires</h3>
+
+				<p>
+
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dignissimos asperiores, voluptates, atque tempore repudiandae fugit hic omnis inventore,
+
+					quisquam expedita minima adipisci. Fugit delectus tempora, ducimus, consectetur quam fugiat! N'es-ce pas ?
+
+				</p>
+
+
+
+				<br /><br />
+
+
+
+				<h2 style="color:#772953">Présentation du site</h2>
+
+
+
+				<h3>L'histoire du site</h3>
+
+
+
+				<p>Comme vous avez pu le voir pour accéder à ce guide, BronyCUB possède un site qui marche à l’heure actuelle sous sa version 2 (V2). Cette version, réalisée solidairement par l'ensemble des membres, remplace la V1 mise en place “à l’arrache” par Heuzef en 2013 et ayant très vite montré ses limites face à l’évolution fulgurante du collectif.</p>
+
+				<p>Bien que la V2 fasse bien son travail, elle nécessite encore de grande améliorations et c’est pour cela que l’un des projet actuel de BronyCUB est la création de la nouvelle version du site : SugarCUB !
+
+					Cette V3 s'appellera ainsi car c’est un outil à part entière qui n’est pas destiné uniquement à BronyCUB. Ce site sera également la base de nombreux autres collectif Brony Français.</p>
+
+					<p>Malheureusement, ce projet est ambitieux et peu de personnes travaillent dessus activement, nous avons fortement besoin d’aide pour accélérer ce projet ! N’hésitez pas a proposer votre aide ici : <a href="https://github.com/bronycub/sugarcub" target="_blank">Github SugarCUB</a></p>
+
+
+
+					<h3>Les Outils</h3>
+
+					<!-- Début des TABS -->
+
+
+
+					<ul class="nav nav-tabs">
+
+						<li class="active"><a href="#mailinglist" data-toggle="tab" aria-expanded="true">Mailing List</a></li>
+
+						<li class=""><a href="#irc" data-toggle="tab" aria-expanded="false">Tchat IRC</a></li>
+
+						<li class=""><a href="#visio" data-toggle="tab" aria-expanded="false">Visio</a></li>
+
+						<li class=""><a href="#agenda" data-toggle="tab" aria-expanded="false">Agenda</a></li>
+
+						<li class=""><a href="#carte" data-toggle="tab" aria-expanded="false">Carte</a></li>
+
+						<li class=""><a href="#gravatar" data-toggle="tab" aria-expanded="false">Gravatar</a></li>
+
+						<li class=""><a href="#plugdj" data-toggle="tab" aria-expanded="false">Plug.DJ</a></li>
+
+						<li class=""><a href="#mc" data-toggle="tab" aria-expanded="false">Serveur Minecraft</a></li>
+
+						<li class=""><a href="#ticket" data-toggle="tab" aria-expanded="false">Ticket</a></li>
+
+					</ul>
+
+
+
+					<div id="myTabContent" class="tab-content">
+
+						<div class="tab-pane fade active in" id="mailinglist">
+
+							<p>La ML (mailing list ou liste de diffusion en Français) à laquelle vous êtes a présent ajouté : bronycub@googlegroups.com permet de communiquer les informations importante lié a la vie du collectifs : meet-up, rendez-vous, partenariat et autres …</p>
+
+							<p>Elle permet de s’organiser et de se tenir au courant de tout les évènements fait par le collectif, elle permet de contacter le collectif dans son intégralité. Elle est d’une importance capitale pour participer à la vie du collectif car sans elle, il est difficile de savoir ce qui se passe. Le chat IRC et autres réseaux sociaux peuvent aider mais ne répondent pas autant à notre besoin, de plus, tout le monde dispose d’une adresse mail, alors que tout le monde n’est pas forcément fan de Facebook et autres Twitter.</p>
+
+							<p>Il est important de mettre en place un filtre dans votre boite e-mail avec le mot clef “BronyCUB” pour gérer efficacement les e-mails en liant avec le collectif car vous allez en recevoir en moyenne 1000 par an. Si vous ne savez pas comment créer un filtre, voici quelques tutos : [INSERT TUTORIAL HERE]</p>
+
+						</div>
+
+						<div class="tab-pane fade" id="irc">
+
+							<p>C’est un salon de conversation. Ce chat permet aux membres de BronyCUB ou d’autres collectifs de venir discuter de manière pratique et simple (c’est mieux que les mails pour la discussion rapide).</p>
+
+							<p>Berry-punch, la mascotte du collectif, est la pour modérer le salon. Un des intérêts du tchat IRC est celui de choisir le logiciel de vos rêve pour rejoindre le salon, mais vous pouvez également rejoindre le salon directement ici.
+
+								Important : il est intéressant d’enregistrer votre pseudo sur le salon de BronyCUB en y tapant directement le message suivant : /msg nickserv register votre_mot_de_passe votre@email.com
+
+								Confirmer ensuite cette demande via le lien que vous aller recevoir par e-mail.</p>
+
+							</div>
+
+							<div class="tab-pane fade" id="visio">
+
+								<p>Si besoin, vous avez à votre disposition un outil de visio très simple à exploiter pour discuter à plusieurs. Il suffit de se rendre sur <a href="https://vroom.im/bronycub" target="_blank">vroom.in/bronycub</a></p>
+
+
+
+							</div>
+
+							<div class="tab-pane fade" id="agenda">
+
+								<p>L’Agenda permet d’avoir a portée toute les informations relatives a un évènement, que ce soit pour ce qui est organisé, ce qui est amené et par qui c’est amené, ce qui peut être amené, etc …</p>
+
+								<p>Si vous souhaitez organiser vous-même quelque chose, il vous suffira alors de cliquer sur “organiser un événement” et de vous laisser guider !</p>
+
+							</div>
+
+							<div class="tab-pane fade" id="carte">
+
+								<p>La carte des membres vous permet simplement d’avoir une vision géographique des Bronies de la CUB et ainsi vous rendre compte que votre voisin est un Brony ;)</p>
+
+
+
+								<p>Nos amis du site d’information Le Poney Blanc, disposent également d’un carte similaire, à l'échelle de la France : <a href="http://carte.leponeyblanc.fr" target="_blank">carte.leponeyblanc.fr</a></p>
+
+							</div>
+
+							<div class="tab-pane fade" id="gravatar">
+
+								<p>Gravatar, c'est un site permettant d’associer a votre pseudo une image. C’est grâce a lui que l’on gère les avatars actuellement. Donc, pour afficher votre avatar, inscrivez-vous sur <a href="http://fr.gravatar.com" target="_blank">Gravatar</a>.</p>
+
+								<p>Si vous avez des difficultés avec ce site, consultez la procédure d’aide Gravatar.</p>
+
+
+
+								<!-- procedure gravatar -->
+
+								<a data-toggle="modal" data-target="#modal_gravatar"><button type="button" class="btn btn-primary">Procédure d'inscription Gravatar</button></a>
+
+
+
+								<div class="modal fade" id="modal_gravatar" tabindex="-1" role="dialog" aria-labelledby="procedure" aria-hidden="true">
+
+									<div class="modal-dialog">
+
+										<div class="modal-content">
+
+											<div class="modal-header">
+
+												<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+
+												<h4 class="modal-title" id="procedure">Procédure d'inscription Gravatar</h4>
+
+											</div>
+
+											<div class="modal-body">
+
+												<ol>
+
+													<li>Sur le site <a href="http://fr.gravatar.com/" target="_blank">gravatar.com</a>, allez en haut à droite pour vous inscrire (<a href="http://i.imgur.com/5tJbVZX.jpg" target="_blank">image</a>)</li>
+
+													<li>Une fois sur la page pour se connecter, cliquez en dessous pour s'inscrire (<a href="http://i.imgur.com/Nh2xN1L.jpg" target="_blank">image</a>)</li>
+
+													<li>Suivez la procédure d'inscription classique (activation du compte, connexion.....)</li>
+
+													<li>Une fois sur votre profil (quand on se connecte) il devrait y avoir déjà votre adresse mail d'enregistrée avec l'image gravatar par défaut, si ce n'est pas le cas cliquez sur "ajouter une nouvelle adresse" pour en rajouter une (<a href="http://i.imgur.com/sJjzALm.jpg" target="_blank">image</a>)</li>
+
+													<li>Une fois que vous avez enregistré votre adresse il vous faut une image! Du coup on fait pareil en dessous (<a href="http://i.imgur.com/BvZEwPy.jpg" target="_blank">image</a>)</li>
+
+													<li>Une fois dans le menu d'image on choisit comment on veut l'uploader, à partir du pc, directement en ligne...</li>
+
+													<li>Quand l'image est choisie il nous permet de la recadrer (<a href="http://i.imgur.com/O3GpUil.jpg" target="_blank">image</a>)</li>
+
+													<li>Une fois l'image bien recadrée on peut valider</li>
+
+													<li>Il nous demande enfin si l'image est "Safe For Work" (que ce n'est pas une image pornographique par exemple) pour choisir il faut cliquer sur une des 4 images en dessous de la nôtre (<a href="http://i.imgur.com/2k4j66W.jpg" target="_blank">image</a>) les explications sur ce que chacune veut dire sont juste en dessous</li>
+
+													<li>Quand c'est choisi on assigne à quelle adresse on veut mettre l'image (<a href="http://i.imgur.com/wJAM3uJ.jpg" target="_blank">image</a>)</li>
+
+												</ol>
+
+											</div>
+
+											<div class="modal-footer">
+
+												<button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
+
+											</div>
+
+										</div>
+
+									</div>
+
+								</div>
+
+								<!-- /procedure gravatar -->
+
+
+
+							</div>
+
+							<div class="tab-pane fade" id="plugdj">
+
+								<p>Plug.DJ est un site qui permet aux membres de BronyCUB de centraliser leurs musiques et ainsi faire une playlist collective que n'importe quel membre peut écouter. C’est très sympatique lors des soirées ! Pour pouvoir accéder et modifier la Playlist des musiques il faut cependant créer et gérer votre compte directement sur ce site.</p>
+
+							</div>
+
+							<div class="tab-pane fade" id="mc">
+
+								<p>Le petit jeu de Lego dématérialisé n'a pas échappé à la vague de petits poneys tout roses ! Et c'est pour cela que notre collectif possède son propre serveur Minecraft !
+
+									Pour pouvoir jouer, il faut d'abord être membres de notre collectif (bien évidemment) et faire la demande à Adriens33 (adrien.sohier@art-software.fr) afin qu'il t’explique le fonctionnement du serveur.<p>
+
+									</div>
+
+									<div class="tab-pane fade" id="ticket">
+
+										<p>Les tickets servent de lien entre l'équipe de gestion et les membres de BronyCUB, ils permettent de lister tous les demandes, problèmes et souhaits du collectif.</p>
+
+										<p>Donc pour toutes demande concernant le collectif, ouvrez un ticket ! Les responsables vous répondrons le plus rapidement possible pour vous aider !</p>
+
+									</div>
+
+
+
+								</div>
+
+
+
+								<!-- Fin des TABS -->
+
+								<br />
+
+								<h2 style="color:#772953">Membres du collectif</h2>
+
+								<p>Pour les membres, comme nous sommes relativement nombreux, chaque personne dispose de sa propre petite fiche de présentation que nous t’invitons à découvrir. Chacune de ces personnes souhaite te connaître et partager ses passions avec toi, alors n’hésite pas à les contacter, c’est comme ça que naissent de belles amitiés.</p>
+
+								<p>Voici certains membres que tu dois absolument connaître si tu souhaite d’investir corps et âme dans l’évolution du collectif :</p>
+
+								<ul>
+
+									<li>Heuzef : Fondateur de BronyCUB.</li>
+
+									<li>Gordfang : Bras droit d’Heuzef, aide précieuse dans la gestion du collectif.</li>
+
+									<li>Adriens33 : Gestion du tchat IRC et du serveur Minecraft.</li>
+
+									<li>Wildfier : Référent du développement de SugarCUB.</li>
+
+									<li>TrappyTickles et Shinsaber79 : Référents de BBBFF.</li>
+
+									<li>SebWhooves : Référent des 23h de la BD.</li>
+
+									<li>Panda : Community Manager du collectif.</li>
+
+									<li>Aragox : Maître des Jeux de Sociétés.</li>
+
+									<li>Ivan Alt : Maître des JDR.</li>
+
+									<li>RunRun : Référent de l’asso Dragons Trésors et Contes.</li>
+
+									<li>Elidyros : Référent Jeux-Vidéos.</li>
+
+									<!-- <li>ValCab33 : Le noob qui a créé cette page.</li> -->
+
+								</ul><br />
+
+
+
+								<h2 style="color:#772953">Nos Amis</h2>
+
+								<p>Au fil des années, BronyCUB vis de sacrées histoires et rencontre pas mal de monde, nous liant d’amitié avec des gens extraordinaires. Chaque entité présente sur notre page d’amis mérite votre attention car nous sommes directement liés à eux.</p><br />
+
+
+
+								<h2 style="color:#772953">Votre Inscription</h2>
+
+								<p>C’est donc le grand moment pour vous de rejoindre le troupeau Bordelais ! Le formulaire d’inscription qui va suivre est là pour nous aider dans la gestion du collectif, il est primordial de le remplir avec soin. Les informations jugées “sensibles” (comme votre N° de tel par exemple) ne sont diffusées qu’en interne, c’est à dire que seul les membres du collectif y auront accès et cela facilite énormément les choses pour la suite.</p>
+
+								<p>N’étant pas une asso, rejoindre le collectif est gratuit et une simple demande de désinscription par ticket suffira si vous déménagez loin par exemple.</p> <!-- faites pas comme moi, partez pas sans prévenir :p -->
+
+
+
+								<p>Le formulaire d’inscription vous demandera vos informations classique (nom, prénom, pseudo, …). Vos informations de contact comme votre téléphone, très pratique pour l’organisation et votre adresse e-mail, qui sera inscrite à la fameuse ML de BronyCUB. Utilisez donc cette même adresse e-mail pour vous créer votre Gravatar.</p>
+
+
+
+								<p>Votre adresse permet de vous situer géographiquement par rapport aux autre et également nous assurez que vous être actif dans la CUB.</p>
+
+								<p>Votre date de naissance pour connaître votre âge mais surtout votre anniversaire ! :D</p>
+
+
+
+								<p>Ensuite, votre bio, il est important d’y mettre au moins paragraphe pour vous présenter aux autres en respectant le format minimum du site. Plus vous êtes bavard mieux c’est ! Inspirez vous des autres présentations par exemple ;) Soyez original, parlez de vos passions et comment vous avez connu BronyCUB et ce que vous en attendez.</p>
+
+
+
+								<p>Enfin, indiquez vos liens (blog, facebook, skype, steam, …) que vous souhaitez partager avec les autres. Puis pour finir vos poneys préférés ;)</p>
+
+
+
+								<p>Une fois fait, cela va créer un ticket et nous reviendrons vers vous pour finaliser votre inscription, c’est à ce moment la qu’il faudra poser toutes vos questions ! A très vite /)</p><br />
+
+
+
+								<a href="http://www.bronycub.org/join.php" class="btn btn-primary btn-lg">Je m'inscrit !</a>
+
+
+
+
 		</div>
 		 <div class="col-md-2">
 		</div>
 		</div>
 	</div>
-
-
-
 
 <?php include 'footer.php'; ?>
