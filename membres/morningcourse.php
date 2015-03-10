8e921f2df4bc88c3203f2caebfa4f167a2e68350
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-10 11:44:51 +0100

    Ajout de morning course

---
+++ membres/morningcourse.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Morning Course";
+$gravatar="4d4312bea3e4f402294522c865dc391c";
+$prenom="Corentin";
+$nom="Roy";
+$naissance="14/07/2000";
+$url="";
+$bio="salut je m’appelle corentin et suis un brony collectionneur de badges mlp mon 
+poney préféré est RAINBOW DASH!!!!! je l’adore car elle est dynamique ,et ne se 
+laisse jamais abattre je peux dire qu’elle me correspond bien.";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
