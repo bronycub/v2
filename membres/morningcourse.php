f3e123cfa8a6e0d957effe98c6f27c97fee50054
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-11 11:00:07 +0100

    Modification de la bio de Morning course

--- membres/morningcourse.php
+++ membres/morningcourse.php
@@ -6,8 +6,10 @@ $prenom="Corentin";
 $nom="Roy";
 $naissance="14/07/2000";
 $url="";
-$bio="Salut je m’appelle corentin et suis un brony collectionneur de badges MLP mon poney préféré est RAINBOW DASH !! Je l’adore car elle est dynamique et ne se
-laisse jamais abattre, je peux dire qu’elle me correspond bien ;)";
+$bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'ai découvert mlp grace a un concours
+ qu'astaen a organisé sur twitter( il y a environ deux mois au moment ou j'écris ca)
+il y avait des badges a gagner(pinkie pie , derpy , et nightmare moon+luna a la manière d'un yin yang) 
+et je l'ai gagné ca m'a donné envie de suivre la série monponey préféré est RAINBOW DASH!!!! je joue souvent a minecraft et autres jeux ou je peux créer toute sortes de choses";
 $pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
