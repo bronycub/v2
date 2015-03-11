4167f50b42654d51d93194dbd71fe5fe2d8107b9
heuzef <contact@heuzef.com>
2015-03-11 12:04:19 +0100

    fix Morning Course ! >.<

--- membres/morningcourse.php
+++ membres/morningcourse.php
@@ -5,18 +5,14 @@ $gravatar="4d4312bea3e4f402294522c865dc391c";
 $prenom="Corentin";
 $nom="Roy";
 $naissance="14/07/2000";
-$url="
-<a href='https://twitter.com/redmoose722' target='_blank' class='zocial icon twitter'></a>
-";
+$url="<a href='https://twitter.com/redmoose722' target='_blank' class='zocial icon twitter'></a>";
 $bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'ai découvert mlp grace a un concours
  qu'astaen a organisé sur twitter( il y a environ deux mois au moment ou j'écris ca)
 il y avait des badges a gagner(pinkie pie , derpy , et nightmare moon+luna a la manière d'un yin yang)
 et je l'ai gagné ca m'a donné envie de suivre la série monponey préféré est RAINBOW DASH!!!! je joue souvent a minecraft et autres jeux ou je peux créer toute sortes de choses";
-$url="";
 $bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'ai découvert MLP grace a un concours
  qu'Astaen a organisé sur Twitter il y avait des badges a gagner (et j'ai gagné) ça m'a donné envie de suivre la série. Je suis fan de RAINBOW DASH !!<br />
  Je joue souvent a Minecraft et autres jeux ou je peux créer toute sortes de choses.";
->>>>>>> 1e76d3e720424244fa45a60ae925d8f1a349b518
 $pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
