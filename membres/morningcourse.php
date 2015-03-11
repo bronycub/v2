0a85ac6cfb3cf0081ac7e6b2daad8922ec2f4920
heuzef <contact@heuzef.com>
2015-03-11 10:26:55 +0100

    update Morning Course

--- membres/morningcourse.php
+++ membres/morningcourse.php
@@ -6,9 +6,8 @@ $prenom="Corentin";
 $nom="Roy";
 $naissance="14/07/2000";
 $url="";
-$bio="salut je m’appelle corentin et suis un brony collectionneur de badges mlp mon 
-poney préféré est RAINBOW DASH!!!!! je l’adore car elle est dynamique ,et ne se 
-laisse jamais abattre je peux dire qu’elle me correspond bien.";
+$bio="Salut je m’appelle corentin et suis un brony collectionneur de badges MLP mon poney préféré est RAINBOW DASH !! Je l’adore car elle est dynamique et ne se
+laisse jamais abattre, je peux dire qu’elle me correspond bien ;)";
 $pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is coolest pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
