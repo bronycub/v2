a23a1665bc3f23ab768b192a6c7c5cc19e17c81c
heuzef <contact@heuzef.com>
2015-03-11 12:01:01 +0100

    fix morning course

--- membres/morningcourse.php
+++ membres/morningcourse.php
@@ -5,13 +5,12 @@ $gravatar="4d4312bea3e4f402294522c865dc391c";
 $prenom="Corentin";
 $nom="Roy";
 $naissance="14/07/2000";
-<<<<<<< HEAD
 $url="
 <a href='https://twitter.com/redmoose722' target='_blank' class='zocial icon twitter'></a>
 ";
 $bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'ai découvert mlp grace a un concours
  qu'astaen a organisé sur twitter( il y a environ deux mois au moment ou j'écris ca)
-il y avait des badges a gagner(pinkie pie , derpy , et nightmare moon+luna a la manière d'un yin yang) 
+il y avait des badges a gagner(pinkie pie , derpy , et nightmare moon+luna a la manière d'un yin yang)
 et je l'ai gagné ca m'a donné envie de suivre la série monponey préféré est RAINBOW DASH!!!! je joue souvent a minecraft et autres jeux ou je peux créer toute sortes de choses";
 =======
 $url="";
