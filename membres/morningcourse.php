e32425af593ec1fbe32b99390bce4a48283165e7
heuzef <contact@heuzef.com>
2015-03-11 12:02:01 +0100

    fix morning course

--- membres/morningcourse.php
+++ membres/morningcourse.php
@@ -12,7 +12,6 @@ $bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'a
  qu'astaen a organisé sur twitter( il y a environ deux mois au moment ou j'écris ca)
 il y avait des badges a gagner(pinkie pie , derpy , et nightmare moon+luna a la manière d'un yin yang)
 et je l'ai gagné ca m'a donné envie de suivre la série monponey préféré est RAINBOW DASH!!!! je joue souvent a minecraft et autres jeux ou je peux créer toute sortes de choses";
-=======
 $url="";
 $bio="Bonjour je me présente je suis corentin (morning course en nom poney) j'ai découvert MLP grace a un concours
  qu'Astaen a organisé sur Twitter il y avait des badges a gagner (et j'ai gagné) ça m'a donné envie de suivre la série. Je suis fan de RAINBOW DASH !!<br />
