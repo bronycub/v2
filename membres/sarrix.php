05a0c1b0b4425d86769ff626334f542d19197c71
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-13 14:56:27 +0100

    Ajout de Tyrris

---
+++ membres/sarrix.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Sarrix";
+$gravatar="5e1e5760547fb0b529f41f5b91895075";
+$prenom="Sébastien";
+$nom="Saint-marc";
+$naissance="05/07/2000";
+$url="";
+$bio="Je suis un petit “geek”. J’adore le codage ^^, les musiques du genre 
+nightcore . J'adore MLP et j'aime bien les oranges :) . Je suis à la recherche 
+d’un groupe de pandas , blagueur et toujours à l'écoute des autres.
+J’ai hâte d’ agir dans l’humanitaire avec vous ! =)";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
