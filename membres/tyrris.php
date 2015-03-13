9879e008455b8ad14721caddc5cb5e7e66c2613b
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-13 14:56:47 +0100

    Ajout de Sarrix

---
+++ membres/tyrris.php
@@ -0,0 +1,18 @@
+<?php //INFOS DU MEMBRES
+//////////////////////////////////////////////
+$pseudo="Tyrris";
+$gravatar="29af690f7a15c67e0e40ff31ef93b4d7";
+$prenom="Richard";
+$nom="Florian";
+$naissance="04/03/1994";
+$url="
+<a href='https://www.facebook.com/N.Tyrris' target='_blank' class='zocial icon facebook'></a>
+<a href='skype:n-tyrris?call' target='_blank' class='zocial icon skype'></a>
+";
+$bio="Pour être tel que je le voudrait j’aurais besoin d’être au moins 20% plus cool …
+Donc pour en venir à moi, je suis étudiant à l’IUT informatique de Bordeaux, je 
+suis un joueur passionné et j’adore les mangas. Je cherche à vous rejoindre pour 
+rencontrez d’autres Bronies et m’amuser dans la joie et la bonne humeur.";
+$pony="<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Princess !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
