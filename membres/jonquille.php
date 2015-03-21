cae21b04f862326874b9a37e12ef77f28b11f7fc
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-21 14:59:10 +0100

    Ajout de Jonquille

---
+++ membres/jonquille.php
@@ -0,0 +1,18 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Jonquille";
+$gravatar="8fc3fedfefebfdf19ac5a0bbe5d682ab";
+$prenom="alexia";
+$nom="sanz de galdeano";
+$naissance="02/05/1990";
+$url="
+<a href='http://www.betaseries.com/membre/Jonquille' target='_blank' class='zocial icon googleplay'></a>
+<a href='https://www.facebook.com/alexia.sanzdegaldeano' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Cinéphile, sérivore, j'en ai fait mon métier en devenant monteuse ! Je suis très ouverte, curieuse de tout, j'aime sortir, rencontrer du monde. 
+J'ai rencontré des bronies au Calice du Dragon, et c'est là que j'ai découvert le collectif. Adorant MLP, j'ai décidé de m'inscrire ici pour rencontrer
+ des gens sympas et intéressants, voir de nouvelles têtes et partager de bons moments :)";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>&nbsp;&nbsp;&nbsp;
+<img src='img/ponies/granny_smith.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Granny smith is best apple !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
