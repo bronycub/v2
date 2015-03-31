58fe1bbe6d6f60df454e5fd5a6f977816d74289a
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-03-31 11:29:46 +0200

    Ajout de Chord tackles

---
+++ membres/chordstackle.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Chords Tackler";
+$gravatar="9c1add7f9dedf1d4831f8899ca50e08e";
+$prenom="Rémy";
+$nom="Leblond";
+$naissance="25/05/1996";
+$url="
+";
+$bio="
+Salut bronies, je suis brony depuis le début 2014 en même temps qu'un de mes amis. On l'est devenu en tombant sur un épisode de la saison 1 à la 
+télévision par hasard. Mon poney préféré est Pinkie Pie car elle est marrante et qu'elle parle beaucoup pour ne rien dire, un peu comme moi. 
+J'aimerai, comme elle, pouvoir faire sourire les gens qui m’entourent. Je suis également fan de Pokemon, j'aime également jouer aux jeux de sociétés 
+comme le Monopoly";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
