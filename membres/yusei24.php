d54081532d921491636ef6deafa0a65e81666405
heuzef <contact@heuzef.com>
2015-03-31 18:20:14 +0200

    correction fiche yusei24

--- membres/yusei24.php
+++ membres/yusei24.php
@@ -8,14 +8,15 @@ $naissance="28/09/1995";
 $url="
 <a href='https://www.facebook.com/raphael.vargas.yusei' target='_blank' class='zocial icon facebook'></a>
 ";
-$bio="Salut à tous, je m'appelle Raphaël, je suis étudiant à l'Université de Bordeaux, en STS ( science, technologie, santé ) 
-,en première année de licence maths/info. Mes passe temps sont les jeux vidéo ( surtout les série de jeux: Pokémon, The legend of Zelda 
-et Kingdom hearts ), les manga/animée ( dont MLP) et les jeux de carte à collectionner ( Yu-Gi-Oh et Magic principalement ). J' ai découvert 
-My Little Pony par un de mes amis qui m'avait passer la première saison, sur le coup j'avais regardé que les deux premiers épisodes, puis j'avais 
-décroché. Je m'y suis remis il y a pas longtemps ( à la sortie de Pokémon ROSA) et je me suis regarder les quatre saison affiler. J'ai découvert 
-BronyCUB, en cherchant ( pour le fun ) des site de rencontre entre Brony, puis j'en ai parlé à des amis qui connaissait et qui mon conseillé de 
-m'inscrire. Mon poney préféré est Fluttershy car étant timide, je me reconnait certaine fois en elle. J’espère bien m'entendre avec vous tous :) 
-et faire des rencontres.";
+$bio="Salut à tous, je m'appelle Raphaël, je suis étudiant à l'Université de Bordeaux, en STS (science, technologie, santé),
+en première année de licence maths/info. Mes passes-temps sont les jeux-vidéos (surtout les séries de jeux : Pokémon, The legend of Zelda
+et Kingdom hearts), les mangas/animées (dont MLP) et les jeux de carte à collectionner (Yu-Gi-Oh et Magic principalement).
+J' ai découvert My Little Pony par un de mes amis qui m'avait passer la première saison, sur le coup, j'avais regardé que les
+deux premiers épisodes, puis j'avais décroché. Je m'y suis remis il y a pas longtemps (à la sortie de Pokémon ROSA) et
+je me suis regarder les quatre saison affiler. J'ai découvert BronyCUB, en cherchant (pour le fun) des sites de rencontre entre Brony,
+puis j'en ai parlé à des amis qui connaissaient et qui mon conseillés de m'inscrire.
+Mon poney préféré est Fluttershy car étant timide, je me reconnait certaine fois en elle.
+J’espère bien m'entendre avec vous tous et faire des rencontres :)";
 $pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
