693a4e7112a82830fdf958365a037af738ae7b2e
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-04-02 23:52:54 +0200

    Ajout de lunayumii

---
+++ membres/lunayumii.php
@@ -0,0 +1,19 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="LunaYumii";
+$gravatar="2c2c3478b4739e23cafe3a370a914af1";
+$prenom="Alexandra";
+$nom="Carreau";
+$naissance="24/01/1995";
+$url="
+<a href='skype:mini-milka-chan?call' target='_blank' class='zocial icon skype'></a>
+<a href='https://www.facebook.com/alecarreau' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="
+Hey ! Je m'appelle Alexandra (Alex ou Lexi la plupart du temps), je suis étudiante en LCE Anglais, passionnée de plein 
+de choses dont les jeux vidéo, le piano, la lecture, la couture, le roleplay et le dessin. A Et si jamais j'entends le mot fanfic, attendez-vous à 
+ce que je ressorte le mode fangirl! Sinon, je me suis mise à MLP y'a pas très longtemps et je suis tombée sous le charme de la folie de Pinkie Pie. Mis à part ça, je vouerais bien un 
+culte à Rainbow Dash aussi. En tout cas, n'hésitez pas à m'ajouter sur FB ou Skype pour discuter! :D";
+$pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
