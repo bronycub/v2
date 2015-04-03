3be472d5910d3b87d73a5cc432017b683cf930b1
heuzef <contact@heuzef.com>
2015-04-03 16:33:45 +0200

    update lunayumii

--- membres/lunayumii.php
+++ membres/lunayumii.php
@@ -10,10 +10,10 @@ $url="
 <a href='https://www.facebook.com/alecarreau' target='_blank' class='zocial icon facebook'></a>
 ";
 $bio="
-Hey ! Je m'appelle Alexandra (Alex ou Lexi la plupart du temps), je suis étudiante en LCE Anglais, passionnée de plein 
-de choses dont les jeux vidéo, le piano, la lecture, la couture, le roleplay et le dessin. A Et si jamais j'entends le mot fanfic, attendez-vous à 
-ce que je ressorte le mode fangirl! Sinon, je me suis mise à MLP y'a pas très longtemps et je suis tombée sous le charme de la folie de Pinkie Pie. Mis à part ça, je vouerais bien un 
-culte à Rainbow Dash aussi. En tout cas, n'hésitez pas à m'ajouter sur FB ou Skype pour discuter! :D";
+Hey ! Je m'appelle Alexandra (Alex ou Lexi la plupart du temps), je suis étudiante en LCE Anglais, passionnée de plein
+de choses dont les jeux vidéo, le piano, la lecture, la couture, le roleplay et le dessin. Si jamais j'entends le mot fanfic, attendez-vous à
+ce que je ressorte le mode fangirl ! Sinon, je me suis mise à MLP y'a pas très longtemps et je suis tombée sous le charme de la folie de Pinkie Pie. Mis à part ça, je vouerais bien un
+culte à Rainbow Dash aussi. En tout cas, n'hésitez pas à m'ajouter sur FB ou Skype pour discuter ! :D";
 $pony="<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
