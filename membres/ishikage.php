5c2003cd62bb6984576b09554c0a08b02b54a9d3
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-05-13 09:37:57 +0200

    Ajout de ishikage

---
+++ membres/ishikage.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Ishikage";
+$gravatar="/98c44b5f00689ea69cb84895b6d4f74c";
+$prenom="Hugo";
+$nom="Soulier";
+$naissance="17/05/1998";
+$url="
+<a href='https://www.facebook.com/hugo.soulier.507' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="salut lui c'est Hugo un mec normal un peu gamer et acro a one piece ( aime aussi pokemon )  jusqu’au jour ou il entre au lycée est rencontre deux personne avec qui il se lira d’amitié 
+malheureusement hugo ce fait vite contaminé par l’univers du manga et deviens un OTAKU de l’Extrême complètement  insociable
+ / la j'y vais un peu fort en fait il est très sociable, aime intéragire  avec les gens  et passez du temps a  s'amusé il est un
+ brony refoulé. Ah oui j'oublié, il a hâte de vous rencontré  ";
+$pony="<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
