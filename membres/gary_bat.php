68b9ae10e3a471af19621baa953079e3fdc22259
heuzef <contact@heuzef.com>
2015-05-18 19:27:15 +0200

    OK Gary Bat

--- membres/gary_bat.php
+++ membres/gary_bat.php
@@ -1,8 +1,8 @@
 <?php //INFOS DU MEMBRE
 //////////////////////////////////////////////
-$pseudo="Gary bat";
+$pseudo="Gary Bat";
 $gravatar="50cf0f654f89c5596d9f3426b8447dd7 ";
-$prenom="Clement";
+$prenom="Clément";
 $nom="Diolot";
 $naissance="10/12/1992";
 $url="
@@ -10,12 +10,10 @@ $url="
 <a href='https://fr-fr.facebook.com/clement.diolot' target='_blank' class='zocial icon facebook'></a>
 <a href='https://www.youtube.com/user/Kidtime89' target='_blank' class='zocial icon youtube'></a>
 ";
-$bio="je suis une personne tres gentille et génereuse ! passionée de cartoon depuis mes 6 ans je dessine des BD et dessins animées depuis plus de 10 ans maintenant .
- étant a l’ESMI ( ecole superieur des metiers de l’ image ) en section charactere animation , je souhaite finir ma vie dans le dessin animée dans des studios telle que
- disney , cartoon network ou même hasbro studio ^^ ( croisons les doigts ) . je suis originaire de bourgogne et connais la série depuis la 2 eme saison . 
- je ne connais pas tout les épisodes j’avoue mais j’adore le graphisme et l’humour de ce dessin animée . Ãtant une personne de confiance vous pouvez compter sur moi 
- en cas de problème . ;) j’aime dessiner faire des vidéo youtube ( chaine kid time ) et surtout la culture geek et imaginaire . ah et j’aime bien les cosplay et le 
- steampunk . voila vous savez tout :D ";
-$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/trixie_lulamoon.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>The great and powerful TRIXIE!</span>";
+$bio="Je suis une personne tres gentille et génereuse ! Passionée de cartoon depuis mes 6 ans je dessine des BD et dessins animées depuis plus de 10 ans maintenant. Étant a l’ESMI (école superieur des metiers de l’ image) en section charactere animation, je souhaite finir ma vie dans le dessin animée dans des studios telle que disney, cartoon network ou même hasbro studio ^^ (croisons les doigts).
+Je suis originaire de bourgogne et connais la série depuis la 2ème saison.
+Je ne connais pas tout les épisodes, j’avoue, mais j’adore le graphisme et l’humour de ce dessin animée. Étant une personne de confiance vous pouvez compter sur moi en cas de problème. ;)
+J’aime dessiner, <a href='https://www.youtube.com/user/Kidtime89'>faire des vidéos youtube</a> et surtout la culture geek et imaginaire. Ah et j’aime bien les cosplays et le steampunk. Voila vous savez tout :D";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/trixie_lulamoon.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>The great and powerful TRIXIE !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
