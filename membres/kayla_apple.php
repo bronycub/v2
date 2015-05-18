599db788393a10ffe88618c9ad084596aaf29bc7
heuzef <contact@heuzef.com>
2015-05-18 19:09:00 +0200

    Kayla Apple OK

---
+++ membres/kayla_apple.php
@@ -0,0 +1,17 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Kayla-Apple";
+$gravatar="7346d6aab57da66a6c41bc22b01c54ea";
+$prenom="Laurine";
+$nom="Paulhiac";
+$naissance="18/08/1995";
+$url="
+<a href='https://www.facebook.com/YumiCosplayEtCompagnie' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Petite cosplayeuse Bordelaise. Fan de My Little Pony et surtout d’Apple Jack ! :D
+N'hésitez pas à visiter <a href='https://www.facebook.com/YumiCosplayEtCompagnie'>ma page Facebook</a>, j'y expose tous mes cosplays !
+J'ai hâte de tous vous rencontrer :D !!!
+";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Apple Jack is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
