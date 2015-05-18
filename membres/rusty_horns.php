92d26020278db0e55ff8e4aca8a7c73bfcf17e99
heuzef <contact@heuzef.com>
2015-05-18 19:15:54 +0200

    OK Rusty Horns

--- membres/rusty_horns.php
+++ membres/rusty_horns.php
@@ -9,15 +9,14 @@ $url="
 <a href='https://twitter.com/rusty_horns' target='_blank' class='zocial icon twitter'></a>
 <a href='http://steamcommunity.com/id/Rusty_Horns/' target='_blank' class='zocial icon steam'></a>
 ";
-$bio=" Voyons voir … Habituellement je ne suis pas un type très sociable, j’aime rester dans mon coins a regarder les gens passer sans dire un mot…
- Mais parfois je sort de mon trous, histoire de voir le monde. Je change de personnalité en fonction du contexte, ce serais pratique si mon arsenal ne se composais 
- pas juste de “homme banal”, “mec que personne ne remarque”, “type qui déblatère sur un sujet qui l'intéresse” et “personnage peut rassurant qui te fixe de loin en 
+$bio=" Voyons voir … Habituellement je ne suis pas un type très sociable, j’aime rester dans mon coins a regarder les gens passer sans dire un mot … Mais parfois je sort de mon trou, histoire de voir le monde. Je change de personnalité en fonction du contexte, ce serais pratique si mon arsenal ne se composais
+ pas juste de “homme banal”, “mec que personne ne remarque”, “type qui déblatère sur un sujet qui l'intéresse” et “personnage peut rassurant qui te fixe de loin en
  faisant des expressions inquiétantes et en disant des trucs morbides” (lui je l’utilise pas souvent).
-J’aime beaucoup le Steampunk, la science et tout le tintouin. J’ai découvert MLP très tôt (vers debut 2011), mais je n’ai jamais vraiment regarder, je me disait 
-“c’est pour les gamines”, jusqu’au jour où je suis devenu amis avec deux Bronys dans ma classe. Ils m’ont fait voir des épisodes… Et j’ai plus lâché depuis!
-Si vous me cherchez IRL, sachez que je ne sort pas souvent de mon terrier, mais je suis surtout aux alentours des lignes A et B. Et si vous êtes au Lycée St-Genês, 
+J’aime beaucoup le Steampunk, la science et tout le tintouin. J’ai découvert MLP très tôt (vers debut 2011), mais je n’ai jamais vraiment regarder, je me disait
+“c’est pour les gamines”, jusqu’au jour où je suis devenu amis avec deux Bronys dans ma classe. Ils m’ont fait voir des épisodes… Et j’ai plus lâché depuis !
+Si vous me cherchez, sachez que je ne sort pas souvent de mon terrier, mais je suis surtout aux alentours des lignes A et B. Et si vous êtes au Lycée St-Genês,
 cherchez la tignasse en mouton dans le hall du lycée pro, 95% de chances que je sois sur un banc ou a cotés.
-(il mâarrive de parler beaucoup pour un introverti) 
+(il m'arrive de parler beaucoup pour un introverti).
 ";
 $pony="<img src='img/ponies/twilight_sparkle.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Twilight Sparkle is best princess !</span>";
 //////////////////////////////////////////////
