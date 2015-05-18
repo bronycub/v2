38b8e042245171f5cf4df797a98e60c5664f7206
heuzef <contact@heuzef.com>
2015-05-18 18:53:46 +0200

    OK Beautifulheart

--- membres/beautifulheart.php
+++ membres/beautifulheart.php
@@ -7,8 +7,7 @@ $nom="BeatifulHeart";
 $naissance="05/11/2000";
 $url="
 ";
-$bio=" Je suis une fille, plutot agréable .. (Enfin je pense x’) ) J’ai toujours envie de rire , je m’associe plutot à Pickie Pie !! x) J’adore les poneys , 
-licornes , tout !!!! Pardon .. Je suis un peu folle dans ma tête , mais c’est sa qui fait que je suis plutot sociable x’) ";
-$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony, yay !</span>&nbsp;&nbsp;<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+$bio=" Je suis une fille, plutot agréable ... (Enfin je pense x’) ) J’ai toujours envie de rire, je m’associe plutot à Pickie Pie !! x) J’adore les poneys, licornes, tout !!!! Pardon ... Je suis un peu folle dans ma tête, mais c’est ça qui fait que je suis plutot sociable x’) ";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony, yay !</span>&nbsp;&nbsp;<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pickie Pie is best pony !</span>";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
