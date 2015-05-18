d29189b0320aed9cea4de7ddda374310472f283b
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-05-18 18:19:40 +0200

    Ajout de BeautifulHeart

---
+++ membres/beautifulheart.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="BeatifulHeart";
+$gravatar="64d5295de2390762098f9030a1f3878b";
+$prenom="Zoé";
+$nom="BeatifulHeart";
+$naissance="05/11/2000";
+$url="
+";
+$bio=" Je suis une fille, plutot agréable .. (Enfin je pense x’) ) J’ai toujours envie de rire , je m’associe plutot à Pickie Pie !! x) J’adore les poneys , 
+licornes , tout !!!! Pardon .. Je suis un peu folle dans ma tête , mais c’est sa qui fait que je suis plutot sociable x’) ";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony, yay !</span>&nbsp;&nbsp;<img src='img/ponies/pinkie_pie.png' class='img-thumbnail'/>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Pinkie Pie is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
