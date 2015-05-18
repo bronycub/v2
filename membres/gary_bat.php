12e51fb6eb8103895496859a0b0571439e3c93ac
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-05-18 18:19:53 +0200

    Ajout de Gary Bat

---
+++ membres/gary_bat.php
@@ -0,0 +1,21 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Gary bat";
+$gravatar="50cf0f654f89c5596d9f3426b8447dd7 ";
+$prenom="Clement";
+$nom="Diolot";
+$naissance="10/12/1992";
+$url="
+<a href='http://garybatstrip.deviantart.com/' target='_blank' class='zocial icon disqus'></a>
+<a href='https://fr-fr.facebook.com/clement.diolot' target='_blank' class='zocial icon facebook'></a>
+<a href='https://www.youtube.com/user/Kidtime89' target='_blank' class='zocial icon youtube'></a>
+";
+$bio="je suis une personne tres gentille et génereuse ! passionée de cartoon depuis mes 6 ans je dessine des BD et dessins animées depuis plus de 10 ans maintenant .
+ étant a l’ESMI ( ecole superieur des metiers de l’ image ) en section charactere animation , je souhaite finir ma vie dans le dessin animée dans des studios telle que
+ disney , cartoon network ou même hasbro studio ^^ ( croisons les doigts ) . je suis originaire de bourgogne et connais la série depuis la 2 eme saison . 
+ je ne connais pas tout les épisodes j’avoue mais j’adore le graphisme et l’humour de ce dessin animée . Ãtant une personne de confiance vous pouvez compter sur moi 
+ en cas de problème . ;) j’aime dessiner faire des vidéo youtube ( chaine kid time ) et surtout la culture geek et imaginaire . ah et j’aime bien les cosplay et le 
+ steampunk . voila vous savez tout :D ";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>AppleJack is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/trixie_lulamoon.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>The great and powerful TRIXIE!</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
