56de5eaee46f38d1640075c6f144e17f1ff4782a
Baptiste Cazaubon <Baptiste.cazaubon@gmail.com>
2015-09-04 15:39:09 +0200

    Ajout de Ink Heart

---
+++ membres/ink_heart.php
@@ -0,0 +1,15 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Ink Heart";
+$gravatar="77473064d4f68ad2b3fc5b4424494819";
+$prenom="Pierre";
+$nom="Fabre";
+$naissance="31/12/1997";
+$url="
+";
+$bio="Yep! Je m'appelle Ink Heart, ou Ink pour faire plus rapide ^.^ Je suis brony depuis environ un an, un tout petit peu plus maintenant, et je viens juste d'emménager à Bordeaux pour mes études.
+ Alors, comme j'avais croisé un paquet de bronys de la BronyCUB à la Ponysouth, je me suis dit que ce serait bête de perdre contact. Voilà, c'est à peu près tout sur pourquoi je suis ici :D 
+Sinon, j'adore le RP, les zèbres, et les substances fermentées! ";
+$pony="<img src='img/ponies/zecora.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Zecora is best zebra !</span>&nbsp;<img src='img/ponies/cloudchaser.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Cloud Chaser is best Pegasus !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
