164af0fba5ae53c2be3f643ad572bf8906655136
Heuzef <contact@heuzef.com>
2016-06-03 02:37:56 +0200

    ajout page 3ans

---
+++ 3ans/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta name="robots" content="index,follow" />
+	<meta name="keywords" content="cub, bronycub.org, heuzef, heuzé, florent, brony, bronie, bronies, pegasister, pegasisters, bordeaux, bordeau, bordo, bordau, bordaux, 33000, 33, gironde, cub, communaute, urbaine, collectif, france, aquitaine, gironde, sud-ouest, mlp, fim, my, little, pony, friendship, is, magic, fan, fans, french, vostfr, fr" />
+	<meta name="description" content="BronyCUB" />
+	<meta name="author" content="Heuzé Florent" />
+	<title>BronyCUB</title>
+	<link href="http://bronycub.org/css_global/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" type="image/png" href="http://bronycub.org/ressources/logo/favicon_bronycub.png" />
+</head>
+
+<body>
+	<div class="container">
+		<div class="span3"></div>
+		<div class="span6">
+			<a href="http://bronycub.org"><img src="http://bronycub.org/ressources/logo/logo_bronycub_min.png" /></a>
+			<h2>BronyCUB 3 ans</h2>
+			<hr />
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Opération reloaded</h3>
+				</div>
+				<div class="panel-body">
+					<p>
+						Fondé le 17 Août 2013, BronyCUB fête chaque été son anniversaire, une solution pour nous retrouver tous ensemble pour profiter un maximum entre amis et tisser des liens avec des membres que nous voyons peut, fêter les anniversaires de tout le monde en même temps, rencontrer des personnes hors collectif, partager des moments mémorables, mettre sur pied des projets importants, ...
+					</p>
+					<p>
+						Pour les 3 ans, nous pouvons encore une fois compter sur la grande générositée des parents de Makhai, qui nous laissent à disposition leur grande maison pour le Weekend du 10 et 11 Septembre 2016, située à Lacanau. Le Transgironde 702 y passe directement depuis la gare Saint-Jean.<br />
+						L'opération reloaded à pour but de fêter les nouveautées à venir, si importantes, qu'elles vont plonger radicalement le collectif dans une nouvelle ère (SugarCUB, BBBFF, La Cave, BerryCon, ...).
+					</p>
+					<p>
+						Rechargement en cours ... Cette fois, on met les bouchées triple !<br />
+						/)
+					</p>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Organisation</h3>
+				</div>
+				<div class="panel-body">
+					<p>
+						L'organisation de l'événement, géré par Heuzef, sera très similaire à celle de l'année dernière :<br />
+						RDV le Vendredi 09 Septembre au <a href="https://encrypted.google.com/maps/@44.9813898,-1.0932751,3a,75y,146.08h,88.04t/data=!3m6!1e1!3m4!1sMhALmkXNu_3GTQHacFpN9g!2e0!7i13312!8i6656">22 allée de planquehaute, 33680 Lacanau</a> dès 18h00 pour profiter de la soirée.<br />
+					</p>
+					<p>
+						Amenez de quoi tenir durant 2 jours :
+						<ul>
+							<li>Couette pour la nuit</li>
+							<li>PC, jeux, ...</li>
+							<li>Maillot de bain, serviette, ...</li>
+							<li>Brosse à dent (brushie, brushie, ...)</li>
+							<li>Balais (sweep, sweep, ...)</li>
+							<li>Bref, tout ce qui vous semble vitale et fun !</li>
+						</ul>
+					</p>
+					<div class="alert alert-dismissible alert-success">
+					  Et pour toute question <strong>➙</strong> contact@heuzef.com / 06 31 32 66 54
+					</div>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Réservation</h3>
+				</div>
+				<div class="panel-body">
+					<p>
+						Comme c'est un événement "à la carte", merci de compléter le formulaire de réservation. Heuzef reprendra contact avec vous directement pour organiser votre séjour.<br />
+						Si l'une de vos connaissances, (hors collectif), souhaite également participer, précisez-le. Vous serez alors garant pour cette personne.<br />
+						Vous avez la possibilité de venir avec plusieurs connaissances, mais leur réservation peuvent-être annulés n'importe quand, en fonction du nombre de places disponibles, (les membres du collectif sont prioritaires).<br />
+						<hr />
+						<a href="http://goo.gl/forms/us6cSa8FWscl1SLA2" class="btn btn-success btn-lg"><strong>Formulaire de réservation</strong></a>
+						<span class="label label-default"><strong>20 places disponibles</strong></span>
+					</p>
+				</div>
+			</div>
+
+			<hr />
+		</div>
+		<div class="span3"></div>
+	</div>
+</body>
+</html>
