76979e92e65780446123ecee1a31cbc66a93a89e
Adrien Sohier <adrien.sohier@art-software.fr>
2016-07-10 20:06:17 +0200

    Mise à jour membre Buddakhiin

--- membres/buddakhiin.php
+++ membres/buddakhiin.php
@@ -10,6 +10,6 @@ $url="
 <a href='https://twitter.com/Buddakhiin' target='_blank' class='zocial icon twitter'></a>
 ";
 $bio="Codeur brony basque, j'arrive sur Bordeaux dans les mois qui suivent dans la joie et la bonne humeur tout en priant la Sainte Trinité : le Piment, la Cafféine, et le Wub.";
-$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is 20% cooler !</span>&nbsp;&nbsp;";
+$pony="<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best Wub !</span>&nbsp;&nbsp;";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
