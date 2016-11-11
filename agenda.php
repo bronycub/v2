844784d0bfcfd63f457ed5d7cd423dd73d3c72e2
Heuzef <contact@heuzef.com>
2016-11-11 14:51:54 +0100

    fix temporaire en attendant SugarCUB !

--- agenda.php
+++ agenda.php
@@ -9,24 +9,7 @@
 
  			 <div class="col-md-6">
           <br /><br />
-          <div class="alert alert-dismissible alert-success">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <i class="fa fa-info-circle fa-1x"></i>&nbsp;Des sessions de JDR ont lieu, chaque samedi après-midi au <a href="qg.php" class="alert-link">QG</a> avec nos amis de chez <a href="http://www.dragonstresorsetcontes.org" class="alert-link">DTC</a> !
-          </div>
- 				<?php
- 				require_once('script/calendar/class/calendar.class.php');
- 				require_once('script/calendar/class/ical.class.php');
-
-				 				$cal = new Calendar(array(
-									 				'url' 			=> "https://www.google.com/calendar/ical/bronycub%40gmail.com/public/basic.ics" ,
-													 				'scale'       		 => 'year' ,
-																	 				'date_day_addition' 	=> false ,
-																					+				'time_format' 				=> 24,
-																					+				'social_links' 				=> false,
-																					 				));
-
-				 				 $cal->output();
-				 				?>
+          <iframe src="https://calendar.google.com/calendar/embed?src=bronycub%40gmail.com&ctz=Europe/Paris" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
  			</div>
 
  			<div class="col-md-3"></div>
