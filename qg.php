109669aba3f704d8a058f290945dda6c96e8fc9e
Funstenolf <sebastien.lefebvrepro@gmail.com>
2016-12-04 17:12:13 +0100

    core: update qg page

--- qg.php
+++ qg.php
@@ -5,13 +5,21 @@
     <h2>Le Quartier Général de BronyCUB !</h2>
     <div class="jumbotron">
       <article>
-        Situé au 132 cours de la Somme 33000 Bordeaux, c'est assez subtile à trouver la première fois.
-        Ouverture prochainement, n'hésitez pas à vous renseigner !
-        <br /><br />
-        <img class="thumbnail img-responsive" src="./img/la_cave.jpg" />
+      	<div class="col-md-offset-3 col-md-6">
+      		<img class="thumbnail img-responsive" src="./img/la_cave.jpg" />
+      	</div>
+      	<div class="clearfix"></div>
+        <b>La Cave</b> est le cœur de l’association Asobu, son QG. C’est un endroit où se mêle bon nombre de gens lié par une passion, la culture dite « geek » et tout ce qui y est rattaché. <br/><br/>
+        L’association propose et organise dans ses locaux plusieurs activités telles que des ateliers cosplay et de pixel art, des marathons cinématographiques ainsi que des meet-up sans oublier ses tables de jeux de rôle. Elle met aussi à disposition de ses membres une large collection de manga, ses consoles de jeux, ses quelques manuels de jeux de rôle et ses jeux de société. La Cave propose aussi un coin café et met donc à disposition gratuitement des boissons chaudes et froides en plus de divers snacks afin de bien passer la journée.<br/><br/>
+        L’ouverture, la fermeture ainsi que la maintenance des locaux de l’association est assuré par ses membres. Les locaux sont ouverts de <b>11h à 18h</b> en temps normal mais sont variables en cas d’évènements ou à la demande des membres.<br/><br/>
+        Bien situé dans Bordeaux, l’accès est simple et bien desservi. La Cave se trouve au <b>132 Cours de la Somme</b> à côté de l’arrêt Fonfrède des bus <span class="badge">5S</span> / <span class="badge">58</span>, à quelques minutes de marche de l’arrêt Lajarte du bus <span class="badge">20</span> et à une bonne dizaine de minutes de marche des arrêts Bergonié / St Nicolas du Tram B et de l’arrêt Victoire des bus <span class="badge">1</span> / <span class="badge">11</span> / <span class="badge">15</span> / <span class="badge">47</span> / <span class="badge">58</span>.<br/><br/>
+        <div class="alert alert-warning">
+        ATTENTION !! La Cave ne fournit aucun alcool et même s’il est possible de ramener ses propres consommations, l’abus d’alcool dans nos locaux est interdit !</div>
+        See you soon /)
         <br />
       </article>
     </div>
+    <a href="https://www.openstreetmap.org/node/2485413677#map=16/44.8296/-0.5706" target="_blank"><img src="./img/la_cave_map.png" class="thumbnail img-responsive"/></a>
   </div>
   <div class="col-md-3"></div>
 </div>
