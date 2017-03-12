a1ac62ea714af08cb0a78278b100f74d128e10cc
Adrien Sohier <adrien.sohier@art-software.fr>
2017-03-12 12:54:13 +0100

    Ajout membre Spinoza

---
+++ membres/spin.php
@@ -0,0 +1,18 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Spinoza";
+$gravatar="5dedca15fbaca3d7a52e1e857473777b ";
+$prenom="Blanche";
+$nom="Jay";
+$naissance="13/12/1996";
+$url="
+<a href='https://www.spin-chan.deviantart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='https://www.youtube.com/user/6spinoza' target='_blank' class='zocial icon youtube'></a>
+<a href='https://www.facebook.com/Frieza.Gold' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Salut ! <br />
+J'ai 20 ans et je vis à Bordeaux. Je suis une fille. <br />
+J'étudie en prépa/math spé, j'aime dessiner et peindre.";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best Pegasus !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
