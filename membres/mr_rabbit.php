88fdd9fd32910376174d95167e82f9ef2b4a176a
Adrien Sohier <adrien.sohier@art-software.fr>
2017-04-03 11:52:44 +0200

    ajout membre Mr Rabbit

---
+++ membres/mr_rabbit.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Mr Rabbit";
+$gravatar="ffa849f26b7ed3ab50e5b6faef76ccaf";
+$prenom="Pierre";
+$nom="Melo";
+$naissance="31/01/2001";
+$url="<a href='https://www.twitch.tv/derpydiversity' target='_blank' class='zocial icon'></a>
+<a href='https://twitter.com/Derpy_Diversity' target='_blank' class='zocial icon twitter'></a>
+<a href='http://steamcommunity.com/id/MrRabbity' target='_blank' class='zocial icon steam'></a>";
+$bio="Bonjour je suis Mr Rabbit, j'aime casser mon clavier sous des coups de rages sur Overwatch. NaCl est mon slogan.";
+$pony="<img src='img/ponies/berry_punch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Berry-Punch is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
