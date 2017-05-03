f46738e68d6ad5e1fa257d38185db7b2753f7fab
Adrien Sohier <adrien.sohier@art-software.fr>
2017-05-03 09:55:46 +0200

    Ajout du membre Godefroy

---
+++ membres/godefroy.php
@@ -0,0 +1,21 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Godefroy";
+$gravatar="8afc97d95b2517ba73e21cf1eefd940f";
+$prenom="Godefroy";
+$nom="Giry";
+$naissance="01/03/2001";
+$url="
+<a href='https://www.facebook.com/profile.php?id=100011689847120&fref=ts' target='_blank' class='zocial icon facebook'></a>
+";
+$bio="Comment dire ? 
+J'avoue c'est une bonne question.  Pour commencer, j'ai découvert mlp en début d'année via Rémi Rollais un de vos membre qui et dans ma classe. 
+Mes passions, j'aime les armes, les bd, les comics, les jeu de rôles,  les jeux de société, enfin les jeux en générale,, la programmation web, l'informatique un peu du coup, la technologie, les culture pop un peu.
+En termes de sport je pratiques le ball-trap, la chasse à cour, le tir à l'arc,  le tir à l'arbalette,  le lancer de couteau, le basket.
+
+J'aime beaucoup les lires les livres de psychologie.
+Mes amis me définnisse comme sociopathe.
+Je suis interne à bordeaux, donc en semaine sur bordeaux.";
+$pony="<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best wub&nbsp;!</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
