f5a2847efb226f98bc05aeaada3591c80b16f2ab
heuzef <contact@heuzef.com>
2017-05-08 23:09:37 +0200

    add Lethanvas

---
+++ membres/lethanvas.php
@@ -0,0 +1,12 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Lethanvas";
+$gravatar="b5ca567e0907a92663fba88ea399c4b8";
+$prenom="Alexis";
+$nom="Fendrich";
+$naissance="18/04/1991";
+$url="";
+$bio="Un fur/brony bordelais. Je dessine à l'occasion et je cherche à prendre contact avec des gens qui ont les mêmes centres d'intérêt.";
+$pony="<img src='img/ponies/rarity.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rarity is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
