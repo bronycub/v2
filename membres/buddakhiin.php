0c714f991e8d5c90f849ccf6768ddf6b4224f910
Florian Pellerin-Bidalot <florian.pellerin.bidalot@gmail.com>
2017-08-06 21:03:18 +0200

    Update buddafiche
    Oui j'avais la flemme d'ouvrir Atom pour faire ça

--- membres/buddakhiin.php
+++ membres/buddakhiin.php
@@ -6,10 +6,10 @@ $prenom="Florian";
 $nom="Pellerin-Bidalot";
 $naissance="30/07/1994";
 $url="
-<a href='http://florianpb.devianart.com' target='_blank' class='zocial icon disqus'></a>
+<a href='https://www.twitch.tv/buddakhiin' target='_blank' class='zocial icon twitch'></a>
 <a href='https://twitter.com/Buddakhiin' target='_blank' class='zocial icon twitter'></a>
 ";
-$bio="Codeur brony basque, j'arrive sur Bordeaux dans les mois qui suivent dans la joie et la bonne humeur tout en priant la Sainte Trinité : le Piment, la Cafféine, et le Wub.";
+$bio="Codeur, streamer, joueur, râleur, buveur, viandard, barbare, caféinomane, mélomane, issu d'un croisement improbable entre le basque, le béarnais, le breton et le catalan, je viens ici pour forger le trône de canettes de Monster.";
 $pony="<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best Wub !</span>&nbsp;&nbsp;";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
