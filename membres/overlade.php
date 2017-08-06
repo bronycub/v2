a6aa5e68c435e831b3c40bb50c6bb73f8f416cc3
Adrien Sohier <adrien.sohier@art-software.fr>
2017-08-06 11:26:33 +0200

    Ajout membre Overlade

---
+++ membres/overlade.php
@@ -0,0 +1,13 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Overlade";
+$gravatar="ec5d9e89c4e9f829792c35594b6a202d";
+$prenom="Brice";
+$nom="Gouilhers";
+$naissance="05/05/2003";
+$url="";
+$bio="Hey !
+Alors je vais me présenter… J'suis fan de MLP, je fais de la musique, et j'aime la culture geek ! ^^";
+$pony="<img src='img/ponies/rainbow_dash.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Rainbow Dash is best wonderbolt !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
