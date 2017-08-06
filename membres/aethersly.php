42f2c1d58f093670f358f27d8aaba0f641f0e281
Adrien Sohier <adrien.sohier@art-software.fr>
2017-08-06 13:04:24 +0200

    Ajout d'Aethersly, qui n'avait pas de fiche.

---
+++ membres/aethersly.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Aethersly";
+$gravatar="81bf239d48103cd8c759325f653c6d8b";
+$prenom="Anon";
+$nom="ymous";
+$naissance="JJ/MM/AAAA";
+$url="<a href='http://aethersly.deviantart.com/' target='_blank' class='zocial icon disqus'></a>";
+$bio="Bonjour ! Je suis un brony de la première heure, ayant découvert la série alors que j'étais dans mes années fac, je n'ai cependant rejoint BronyCUB que quelques années après (le temps de découvrir que le collectif existait eeet… de franchir le pas ^^)
+
+Ex-étudiant en art (diplômé mais pas doué !), je dessine de temps en temps dans un style manga ou poney et j'ai un DeviantArt (<a href='http://aethersly.deviantart.com'>lien</a>). Je suis également fan d'anime et de jeux vidéos, avec une petite expérience de game/level designer dans les pattes (et un mémoire qui porte dessus !). À coté de cela, j'essaie d'être compréhensif, médiateur et vertueux (d'où mon affinité avec la philosophie brony ^^), bien qu'étant prudent et casanier.";
+$pony="<img src='img/ponies/fluttershy.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Fluttershy is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/luna.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Luna is best Night Princess ~</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
