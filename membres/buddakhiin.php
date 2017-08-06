3bba9bc722a84b3fecc47c7ca3a9bb4e4dbd8d1a
Florian Pellerin-Bidalot <florian.pellerin.bidalot@gmail.com>
2017-08-06 21:21:04 +0200

    Test commit avec Atom (ajouts sur ma fiche)

--- membres/buddakhiin.php
+++ membres/buddakhiin.php
@@ -9,7 +9,7 @@ $url="
 <a href='https://www.twitch.tv/buddakhiin' target='_blank' class='zocial icon twitch'></a>
 <a href='https://twitter.com/Buddakhiin' target='_blank' class='zocial icon twitter'></a>
 ";
-$bio="Codeur, streamer, joueur, râleur, buveur, viandard, barbare, caféinomane, mélomane, issu d'un croisement improbable entre le basque, le béarnais, le breton et le catalan, je viens ici pour forger le trône de canettes de Monster.";
+$bio="Codeur, streamer, rédacteur, testeur, joueur, râleur, buveur, viandard, barbare, caféinomane, mélomane, issu d'un croisement improbable entre le basque, le béarnais, le breton et le catalan, je viens ici pour forger le trône de canettes de Monster.";
 $pony="<img src='img/ponies/vinyl_scratch.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Vinyl Scratch is best Wub !</span>&nbsp;&nbsp;";
 //////////////////////////////////////////////
 include "template_membres.php"; ?>
