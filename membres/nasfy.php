45898db43fd7a000da63caac5d808b8589f98334
Adrien Sohier <adrien.sohier@art-software.fr>
2017-09-04 09:18:46 +0200

    Ajout du membre Nasfy

---
+++ membres/nasfy.php
@@ -0,0 +1,14 @@
+<?php //INFOS DU MEMBRE
+//////////////////////////////////////////////
+$pseudo="Nasfy";
+$gravatar="e17afa45e05e96f18fa34452f1d69213";
+$prenom="Josuald";
+$nom="Pic";
+$naissance="02/04/1998";
+$url="";
+$bio="Bonjour,<br />
+Brony depuis 2014, je fais un peu de jeu de rôle avec mes amis. Je suis fan de J.R.R. Tolkien, d'anime, de dessins animés, de beaucoup de trucs geek, d'hypnose, mais surtout de jeux de société.<br />
+Voilà à peu près tout :)";
+$pony="<img src='img/ponies/apple_jack.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Applejack is best pony !</span>&nbsp;&nbsp;<img src='img/ponies/starlight_glimmer.png' class='img-thumbnail'>&nbsp;&nbsp;&nbsp;<span class='label label-info'>Starlight Glimmer is best pony !</span>";
+//////////////////////////////////////////////
+include "template_membres.php"; ?>
