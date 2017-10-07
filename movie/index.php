4c41996b46ad54889072fa2204ed4449d2d10eda
Funstenolf <sebastien.lefebvrepro@gmail.com>
2017-10-08 00:04:43 +0200

    feat: adding new event page for the movie

---
+++ movie/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="robots" content="index,follow" />
+		<meta name="keywords" content="cub, bronycub.org, heuzef, heuzé, florent, brony, bronie, bronies, pegasister, pegasisters, bordeaux, bordeau, bordo, bordau, bordaux, 33000, 33, gironde, cub, communaute, urbaine, collectif, france, aquitaine, gironde, sud-ouest, mlp, fim, my, little, pony, friendship, is, magic, fan, fans, french, vostfr, fr" />
+		<meta name="description" content="BronyCUB" />
+		<meta name="author" content="Heuzé Florent" />
+		<title>BronyCUB - The Movie</title>
+		<link href="http://bronycub.org/css_global/css/bootstrap.css" rel="stylesheet" type="text/css">
+		<link rel="shortcut icon" type="image/png" href="http://bronycub.org/ressources/logo/favicon_bronycub.png" />
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="span3"></div>
+			<div class="span6">
+				<a href="http://bronycub.org"><img src="http://bronycub.org/ressources/logo/logo_bronycub_min.png" /></a>
+				<h2>BronyCUB - MLP The Movie</h2>
+				<hr />
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/ElLAOrHyhp4" frameborder="0" allowfullscreen></iframe>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Opération The Movie</h3>
+					</div>
+					<div class="panel-body">
+						<p>
+							<div class="alert alert-dismissible alert-danger" align="center">
+								<strong>ÉVÉNEMENT EXCEPTIONNEL MIRACULEUX</strong><br/>
+								Le film <i>MLP: The Movie</i> en haute qualité <u>VO</u> a fuité, avant sa sortie, et on va pas se gêner pour en profiter ! ;)<br/>
+								On vous invite donc à venir le voir en avant-avant première (même ceux qui se sont divulgaché avec la VF pourront venir).
+							</div>
+						</p>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Organisation</h3>
+					</div>
+					<div class="panel-body">
+						<p>
+							L'organisation de l'événement, géré par Heuzef et Lightning_Bass, se déroulera dans la joie et la bonne humeur !<br />
+							RDV le Vendredi 13 Octobe chez Heuzef dès 18h00 pour profiter de la soirée.<br />
+						</p>
+						<p>
+							L'événement est en mode auberge espagnole : apportez du miam et du glou, mais aussi une chaise (car il en manquera) si vous en avez la possibilité.
+						</p>
+						<p>
+							<ul>
+								<li><a href="https://encrypted.google.com/maps/dir//80+Rue+des+Orangers,+33200+Bordeaux,+France/@44.8371377,-0.5844547,14.12z/data=!4m16!1m7!3m6!1s0xd54d7ecfda02b87:0x44d54a19498115e9!2s80+Rue+des+Orangers,+33200+Bordeaux,+France!3b1!8m2!3d44.8609045!4d-0.6183452!4m7!1m0!1m5!1m1!1s0xd54d7ecfda02b87:0x44d54a19498115e9!2m2!1d-0.6183452!2d44.8609045">80 Rue des Orangers, 33200 Bordeaux</a></li>
+								<li><a href="https://encrypted.google.com/maps/@44.8610305,-0.6184285,3a,79.1y,176.86h,88.76t/data=!3m6!1e1!3m4!1s7NoZ0jgcA8E03YOkHuUYOw!2e0!7i13312!8i6656">Lien Street View</a></li>
+								<li>Deuxième entrée (la porte sera ouverte)</li>
+								<li>Montez au dernier étage</li>
+							</ul>
+						</p>
+						<div class="alert alert-dismissible alert-success">
+						  Et pour toute question <strong>➙</strong> contact@heuzef.com / 06 31 32 66 54
+						</div>
+					</div>
+				</div>
+
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3 class="panel-title">Participation</h3>
+					</div>
+					<div class="panel-body">
+						<p>
+							Pour signaler votre venue, merci de l'indiquer sur le Framadate.<br />
+							Si l'une de vos connaissances, (hors collectif), souhaite également participer, précisez-le. Vous serez alors garant pour cette personne.<br />
+							L'événement reste tout de même limité à une dizaine de personnes par soucis de place.
+							<hr />
+							<a href="https://framadate.org/wzCk9z9BHGH5EThD" class="btn btn-success btn-lg"><strong>Participer</strong></a>
+						</p>
+					</div>
+				</div>
+
+				<hr />
+			</div>
+			<div class="span3"></div>
+		</div>
+	</body>
+</html>
